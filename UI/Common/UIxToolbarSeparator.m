b653b4d8e2bb7015d9e44edcdab886e513b8a431
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-07-05 21:15:21 +0000

    - new toolbar code and images;
    Monotone-Parent: cd6e43ebd8d873923fb9ad3c40158a6ce63a1f09
    Monotone-Revision: 21ae9d2d632db73281a8277df02900d3e48a6e98
    
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-07-05T21:15:21
    Monotone-Branch: ca.inverse.sogo

---
+++ UI/Common/UIxToolbarSeparator.m
@@ -0,0 +1,14 @@
+#import <Foundation/NSObject.h>
+#import <Foundation/NSString.h>
+
+#import <SOGoUI/UIxComponent.h>
+
+@interface UIxToolbarSeparator : UIxComponent
+{
+}
+
+@end
+
+@implementation UIxToolbarSeparator
+
+@end
