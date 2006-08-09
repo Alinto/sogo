8184e503ef884b5d229a9bf66e5cce8887b749c0
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-08-09 21:23:57 +0000

    Monotone-Parent: bc323b6b1daa8bf2032ce4ebb7f5596ad96cba2f Monotone-Revision: 568c8c16e2bbc7c2411420420f39d105079c1cd0
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-08-09T21:23:57
    Monotone-Branch: ca.inverse.sogo

---
+++ SoObjects/Contacts/SOGoContactFolder.h
@@ -0,0 +1,70 @@
+/*
+  Copyright (C) 2004-2005 SKYRIX Software AG
+
+  This file is part of OpenGroupware.org.
+
+  OGo is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+
+#ifndef __Contacts_SOGoContactFolder_H__
+#define __Contacts_SOGoContactFolder_H__
+
+/*
+  SOGoContactFolder
+    Parent object: the user's SOGoUserFolders
+    Child objects: SOGoContactObject
+
+  The SOGoContactFolder maps to an GCS folder of type 'contact', that
+  is, a content folder containing vcal?? files (and a proper quicktable).
+*/
+
+#import <Foundation/NSObject.h>
+
+@class NSString, NSArray;
+@class SOGoContactObject;
+@class SOGoObject;
+
+@protocol SOGoContactObject;
+
+#import <SoObjects/SOGo/SOGoFolder.h>
+
+#define folderListingFields [NSArray arrayWithObjects: @"c_name", @"cn", \
+                                     @"sn", @"givenname", @"l",          \
+                                     @"mail", @"telephonenumber",        \
+                                     nil]
+
+@protocol SOGoContactFolder <NSObject>
+
++ (id <SOGoContactFolder>) contactFolderWithName: (NSString *) aName
+                                  andDisplayName: (NSString *) aDisplayName
+                                     inContainer: (SOGoObject *) aContainer;
+
+- (id <SOGoContactFolder>) initWithName: (NSString *) aName
+                         andDisplayName: (NSString *) aDisplayName
+                            inContainer: (SOGoObject *) aContainer;
+
+
+- (id <SOGoContactObject>) lookupContactWithId: (NSString *) recordId;
+- (NSArray *) lookupContactsWithFilter: (NSString *) filter
+                                sortBy: (NSString *) sortKey
+                              ordering: (NSComparisonResult) sortOrdering;
+
+- (void) setDisplayName: (NSString *) aDisplayName;
+- (NSString *) displayName;
+
+@end
+
+#endif /* __Contacts_SOGoContactFolder_H__ */
