3c07b3040bf02a571cf274a9306a2af92d087626
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-08-09 21:14:38 +0000

    Monotone-Parent: c29edea33f25c719bad972e23d7441b3851b8164 Monotone-Revision: 9ddb8a8c0200ef2ba32370aff1ace4928f5fad1c
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-08-09T21:14:38
    Monotone-Branch: ca.inverse.sogo

---
+++ SoObjects/Contacts/SOGoContactGCSEntry.m
@@ -0,0 +1,96 @@
+/*
+  Copyright (C) 2004-2005 SKYRIX Software AG
+
+  This file is part of OpenGroupware.org.
+
+  OGo is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+
+#import <Foundation/NSArray.h>
+#import <Foundation/NSString.h>
+
+#import <NGiCal/NGVCard.h>
+
+#import "NGVCard+Contact.h"
+
+#import "SOGoContactGCSEntry.h"
+
+@implementation SOGoContactGCSEntry
+
+- (id) init
+{
+  if ((self = [super init]))
+    {
+      card = nil;
+    }
+
+  return self;
+}
+
+- (void) dealloc
+{
+  if (card)
+    [card release];
+  [super dealloc];
+}
+
+/* content */
+
+- (NGVCard *) vCard
+{
+  NSArray *cards;
+  NSString *contentStr;
+
+  if (!card)
+    {
+      contentStr = [self contentAsString];
+      if ([contentStr hasPrefix:@"BEGIN:VCARD"])
+        {
+          cards = [NGVCard parseVCardsFromSource: contentStr];
+          if ([cards count] > 0)
+            card = [cards objectAtIndex: 0];
+        }
+    }
+
+  return card;
+}
+
+/* DAV */
+
+- (NSString *) davContentType
+{
+  return @"text/x-vcard";
+}
+
+/* specialized actions */
+
+- (NSException *) save
+{
+  NGVCard *vcard;
+
+  vcard = [self vCard];
+
+  return [self saveContentString: [vcard asString]];
+}
+
+/* message type */
+
+- (NSString *) outlookMessageClass
+{
+  return @"IPM.Contact";
+}
+
+@end /* SOGoContactGCSEntry */
