962120c7c35ff8b66b5a443dd3def3d349f18b77
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-08-18 22:50:15 +0000

    Monotone-Parent: 9396acfed8c54ce550e7db6b46c96c7b9006d532 Monotone-Revision: d77760b82142bf6467142bd190255019d19263e0
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-08-18T22:50:15
    Monotone-Branch: ca.inverse.sogo

---
+++ UI/Scheduler/NSCalendarDate+Scheduler.h
@@ -0,0 +1,33 @@
+/* NSCalendarDate+Scheduler.h - this file is part of SOGo
+   Copyright (C) 2000-2004 SKYRIX Software AG
+
+   This file is part of OGo
+
+   OGo is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+   License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with OGo; see the file COPYING.  If not, write to the
+   Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+*/
+
+#ifndef NSCALENDARDATE_SCHEDULER_H
+#define NSCALENDARDATE_SCHEDULER_H
+
+#import <Foundation/NSCalendarDate.h>
+
+@interface NSCalendarDate (UIxCalMonthOverviewExtensions)
+
+- (BOOL) isDateInSameMonth: (NSCalendarDate *) _other;
+
+@end
+
+#endif /* NSCALENDARDATE_SCHEDULER_H */
