69c04122ecf804303a7a1a7f18b8a3261e133fa5
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-08-18 22:52:55 +0000

    Monotone-Parent: 9bf9ff8b6daaabd45f42c3278a9e48a54484d5f6 Monotone-Revision: 3bb55594abc16b51ed95faf0b56f29ec191fe8ac
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-08-18T22:52:55
    Monotone-Branch: ca.inverse.sogo

---
+++ UI/Scheduler/UIxCalFilterPanel.m
@@ -0,0 +1,118 @@
+/* UIxContactsFilterPanel.m - this file is part of SOGo
+ *
+ * Copyright (C) 2006 Inverse groupe conseil
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#import <Foundation/NSArray.h>
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSKeyValueCoding.h>
+
+#import <NGObjWeb/WOContext.h>
+
+#import "UIxCalFilterPanel.h"
+
+static NSArray *filters = nil;
+
+@implementation UIxCalFilterPanel
+
++ (void) initialize
+{
+  NSMutableDictionary *md;
+  NSMutableArray *ma;
+  
+  md = [[NSMutableDictionary alloc] initWithCapacity:8];
+  ma = [[NSMutableArray alloc] initWithCapacity:4];
+
+  filters           = [ma copy];
+  [md release]; md = nil;
+  [ma release]; ma = nil;
+}
+
+- (id) init
+{
+  if ((self = [super init]))
+    {
+      searchText = nil;
+      searchCriteria = nil;
+    }
+
+  return self;
+}
+
+- (void) dealloc
+{
+  [self->searchCriteria release];
+  [self->searchText release];
+  [super dealloc];
+}
+
+/* accessors */
+
+- (void) setSearchText: (NSString *)_txt
+{
+  ASSIGNCOPY(self->searchText, _txt);
+}
+
+- (void) setSearchCriteria: (NSString *)_txt
+{
+  ASSIGNCOPY(self->searchText, _txt);
+}
+
+- (NSString *) searchText
+{
+  if (!searchText)
+    searchText = [[self queryParameterForKey: @"search"] copy];
+
+  return searchText;
+}
+
+- (NSString *) searchCriteria
+{
+  if (!searchCriteria)
+    searchCriteria = [[self queryParameterForKey: @"criteria"] copy];
+
+  return searchCriteria;
+}
+
+/* filters */
+
+- (NSArray *) filters
+{
+  return filters;
+}
+
+/* qualifiers */
+
+- (NSString *) filterLabel
+{
+#if 1
+  return [[[self context] page] labelForKey: [self valueForKey:@"filter"]];
+#else
+  return [self valueForKey: @"filter"];
+#endif
+}
+
+- (NSString *) selectedFilter
+{
+  return [self queryParameterForKey: @"filterpopup"];
+}
+
+@end /* UIxCalFilterPanel */
