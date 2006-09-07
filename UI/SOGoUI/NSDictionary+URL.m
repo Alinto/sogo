d854821b721cd749111c17d0d4ca9a73dd0524b7
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-09-07 19:14:47 +0000

    Monotone-Parent: 4ce1a15200da85e7f90d8d248cd95b6615bc707e Monotone-Revision: 6d36e54f1eaf29904dfc5704ceda7ec96bfe06a1
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-09-07T19:14:47
    Monotone-Branch: ca.inverse.sogo

--- UI/SOGoUI/NSDictionary+URL.m
+++ UI/SOGoUI/NSDictionary+URL.m
@@ -20,8 +20,9 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#import <Foundation/NSString.h>
+#import <Foundation/NSArray.h>
 #import <Foundation/NSEnumerator.h>
+#import <Foundation/NSString.h>
 
 #import "NSDictionary+URL.h"
 
