65f05bad534954a16beda20672dfb48a6208d982
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2006-10-03 14:36:03 +0000

    Monotone-Parent: d48095cca5ce95803d881d96312e4b396f275daf Monotone-Revision: 9ffc575e66dc27e09f7df66d4063ceae42655bf0
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2006-10-03T14:36:03
    Monotone-Branch: ca.inverse.sogo

--- UI/Common/UIxPageFrame.m
+++ UI/Common/UIxPageFrame.m
@@ -61,7 +61,10 @@
 
 - (NSString *) doctype
 {
-  return @"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">";
+  return (@"<?xml version=\"1.0\"?>\n"
+          @"<!DOCTYPE html"
+          @" PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
+          @" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
 }
 
 /* Help URL/target */
