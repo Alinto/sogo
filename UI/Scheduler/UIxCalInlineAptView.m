89c57884ed8519d3c08edbf17c37d9881fd2221b
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-03-02 22:30:11 +0000

    Monotone-Parent: 5de515c6a9999e54a1363b035e781e2fb581975f Monotone-Revision: 873696f0482e8408c8fda6ce2156c1afc61c2e60
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-03-02T22:30:11
    Monotone-Branch: ca.inverse.sogo

--- UI/Scheduler/UIxCalInlineAptView.m
+++ UI/Scheduler/UIxCalInlineAptView.m
@@ -18,7 +18,7 @@
  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
  02111-1307, USA.
  */
-// $Id: UIxCalInlineAptView.m 1018 2007-02-08 16:37:00Z wolfgang $
+// $Id: UIxCalInlineAptView.m 1029 2007-03-02 22:28:13Z wolfgang $
 
 #import <math.h>
 
