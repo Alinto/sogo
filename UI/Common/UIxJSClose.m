596048ace406fb94886f78aeb6702162e7786b30
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-04-16 21:40:45 +0000

    Monotone-Parent: 07a28abc844008a513555a2e67b3e7e410b3f355 Monotone-Revision: 07ace1b999a3540691535081e95bb20d766899c7
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-04-16T21:40:45
    Monotone-Branch: ca.inverse.sogo

---
+++ UI/Common/UIxJSClose.m
@@ -0,0 +1,71 @@
+/* UIxJSClose.m - this file is part of SOGo
+ *
+ * Copyright (C) 2006 Inverse groupe conseil
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#import <Foundation/NSString.h>
+
+#import "UIxJSClose.h"
+
+@implementation UIxJSClose
+
+- (id) init
+{
+  if ((self = [super init]))
+    refreshMethod = nil;
+
+  return self;
+}
+
+- (void) dealloc
+{
+  if (refreshMethod)
+    [refreshMethod release];
+  [super dealloc];
+}
+
+- (NSString *) doctype
+{
+  return (@"<?xml version=\"1.0\"?>\n"
+          @"<!DOCTYPE html"
+          @" PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
+          @" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+}
+
+- (void) setRefreshMethod: (NSString *) method
+{
+  if (refreshMethod)
+    [refreshMethod release];
+  refreshMethod = method;
+  if (refreshMethod)
+    [refreshMethod retain];
+}
+
+- (NSString *) refreshMethod
+{
+  return refreshMethod;
+}
+
+- (BOOL) hasRefreshMethod
+{
+  return (refreshMethod != nil);
+}
+
+@end
