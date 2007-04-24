8bd4abeaf789b3f5c7ea143350cb7f4e842a2783
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-04-24 23:04:36 +0000

    Monotone-Parent: 666cdfc6838a3a3654e9b05b044718898511cd66 Monotone-Revision: e96676c1d053a9dc539df8580b1176640559093d
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-04-24T23:04:36
    Monotone-Branch: ca.inverse.sogo

--- UI/Scheduler/UIxCalUserRightsEditor.m
+++ UI/Scheduler/UIxCalUserRightsEditor.m
@@ -20,6 +20,8 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#import <Foundation/NSArray.h>
+#import <Foundation/NSEnumerator.h>
 #import <NGObjWeb/WORequest.h>
 #import <SoObjects/SOGo/SOGoPermissions.h>
 
