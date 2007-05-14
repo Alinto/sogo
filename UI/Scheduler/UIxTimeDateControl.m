68aadd5f7ffb352b13997701ac1219ba3223c987
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-05-14 17:27:01 +0000

    Monotone-Parent: 60cdea034d12ae4d4566e2d0ba49d506bff79ef2 Monotone-Revision: cf8c187bd6389eaf744f04cbf46806573f18fc89
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-05-14T17:27:01
    Monotone-Branch: ca.inverse.sogo

--- UI/Scheduler/UIxTimeDateControl.m
+++ UI/Scheduler/UIxTimeDateControl.m
@@ -18,7 +18,6 @@
   Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
   02111-1307, USA.
 */
-// $Id: UIxTimeDateControl.m 1045 2007-04-11 20:13:07Z wolfgang $
 
 #import <Foundation/NSArray.h>
 #import <Foundation/NSString.h>
