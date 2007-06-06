cd9812ed9d34721e2a3985f2552ec5b1d13b552a
Francis Lachapelle <flachapelle@inverse.ca>
2007-06-06 20:42:43 +0000

    Monotone-Parent: 5f682c49b205052d24f979efb24d2bbca2ed1f7c Monotone-Revision: 1c22eb30b09a0d3629c623199f34e18685dc301a
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2007-06-06T20:42:43
    Monotone-Branch: ca.inverse.sogo

---
+++ SoObjects/SOGo/NSNumber+Utilities.h
@@ -0,0 +1,36 @@
+/* NSNumber+Utilities.h - this file is part of SOGo
+ *
+ * Copyright (C) 2007 Inverse groupe conseil
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef NSNUMBER_URL_H
+#define NSNUMBER_URL_H
+
+#import <Foundation/NSValue.h>
+
+@class NSString;
+
+@interface NSNumber (SOGoURLExtension)
+
+- (NSString *) jsonRepresentation;
+
+@end
+
+#endif /* NSNUMBER_URL_H */
