4afcd844023269920d1eaef13317a20c733b73d1
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-06-29 21:54:04 +0000

    Monotone-Parent: 4480158eb35ae79e7ba3cbb9595c24ef6b6f020f Monotone-Revision: 0853417306cf8e081b4e93f4b80e5feb8e96cdba
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-06-29T21:54:04
    Monotone-Branch: ca.inverse.sogo

--- UI/MainUI/AgenorProfile.sql
+++ UI/MainUI/AgenorProfile.sql
@@ -1,5 +1,6 @@
 --
--- (C) 2005 SKYRIX Software AG
+-- (C) 2004-2005 SKYRIX Software AG
+-- (C) 2006-2007 Inverse groupe conseil
 --
 
 CREATE TABLE SOGo_user_profile (
--- UI/MainUI/OCSFolderInfo.sql
+++ UI/MainUI/OCSFolderInfo.sql
@@ -1,20 +1,10 @@
 --
 -- (C) 2004-2005 SKYRIX Software AG
+-- (C) 2006-2007 Inverse groupe conseil
 --
--- TODO:
---   add a unique constraints on path
-
-DROP SEQUENCE SOGo_folder_info_seq;
-
-CREATE SEQUENCE SOGo_folder_info_seq;
-
-DROP TABLE SOGo_folder_info;
 
 CREATE TABLE SOGo_folder_info (
-  c_folder_id  INTEGER 
-    DEFAULT nextval('SOGo_folder_info_seq')
-    NOT NULL 
-    PRIMARY KEY,                     -- the primary key
+  c_folder_id      SERIAL,
   c_path           VARCHAR(255)  NOT NULL, -- the full path to the folder
   c_path1          VARCHAR(255)  NOT NULL, -- parts (for fast queries)
   c_path2          VARCHAR(255)  NULL,     -- parts (for fast queries)
@@ -23,6 +13,6 @@ CREATE TABLE SOGo_folder_info (
   c_foldername     VARCHAR(255)  NOT NULL, -- last path component
   c_location       VARCHAR(2048) NOT NULL, -- URL to folder
   c_quick_location VARCHAR(2048) NULL,     -- URL to quicktable of folder
-  c_acl_location VARCHAR(2048) NULL,     -- URL to quicktable of folder
+  c_acl_location   VARCHAR(2048) NULL,     -- URL to quicktable of folder
   c_folder_type    VARCHAR(255)  NOT NULL  -- the folder type ...
 );
