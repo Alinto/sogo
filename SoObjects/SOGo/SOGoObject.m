4e6586bf4a78ad11845d0e77be00f7f2b7743a55
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-07-30 15:56:58 +0000

    Monotone-Parent: b84ec9dfa896541bccb01ef1607b78353b63bad0 Monotone-Revision: a08ab23d521b24b5495493a2b4c03707186b438e
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-07-30T15:56:58
    Monotone-Branch: ca.inverse.sogo

--- SoObjects/SOGo/SOGoObject.m
+++ SoObjects/SOGo/SOGoObject.m
@@ -167,10 +167,10 @@ static BOOL kontactGroupDAV = YES;
 //   SoClass security declarations
   
 //   require View permission to access the root (bound to authenticated ...)
-  [[self soClassSecurityInfo] declareObjectProtected: SoPerm_View];
+//   [[self soClassSecurityInfo] declareObjectProtected: SoPerm_View];
 
 //   to allow public access to all contained objects (subkeys)
-  [[self soClassSecurityInfo] setDefaultAccess: @"allow"];
+//   [[self soClassSecurityInfo] setDefaultAccess: @"allow"];
   
 //   /* require Authenticated role for View and WebDAV */
 //   [[self soClassSecurityInfo] declareRole: SoRole_Owner
