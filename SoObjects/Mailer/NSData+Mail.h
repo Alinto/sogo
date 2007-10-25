746063a94038d872f1bbc1602dd201ba88e9f6ec
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-10-25 15:23:17 +0000

    Monotone-Parent: 7ee0c1dd084226bd61d44f69428903723aac6bbc Monotone-Revision: 99aecefc3b45bf70dccf3bc8c5f60e6017e3935b
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-10-25T15:23:17
    Monotone-Branch: ca.inverse.sogo

---
+++ SoObjects/Mailer/NSData+Mail.h
@@ -0,0 +1,36 @@
+/* NSData+Mail.h - this file is part of SOGo
+ *
+ * Copyright (C) 2007 Inverse groupe conseil
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef NSDATA_MAIL_H
+#define NSDATA_MAIL_H
+
+#import <Foundation/NSData.h>
+
+@class NSString;
+
+@interface NSData (SOGoMailUtilities)
+
+- (NSData *) bodyDataFromEncoding: (NSString *) encoding;
+
+@end
+
+#endif /* NSDATA_MAIL_H */
