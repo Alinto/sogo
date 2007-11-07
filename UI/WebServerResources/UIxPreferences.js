4984db856388f680a080ab5f5407c35de84c3329
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2007-11-07 17:40:16 +0000

    Monotone-Parent: 88c5085f8ba96b5bcd3673fdb4d97d3db679ba9b Monotone-Revision: d38b82f4d070ced8ba970b79c220008c590b1e3d
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2007-11-07T17:40:16
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/UIxPreferences.js
+++ UI/WebServerResources/UIxPreferences.js
@@ -3,21 +3,3 @@ function savePreferences(sender) {
 
    return false;
 }
-
-function initPreferences() {
-  var identitiesBtn = $("manageIdentitiesBtn");
-  Event.observe(identitiesBtn, "click",
-		popupIdentitiesWindow.bindAsEventListener(identitiesBtn));
-}
-
-function popupIdentitiesWindow(event) {
-  var urlstr = UserFolderURL + "identities";
-  var w = window.open(urlstr, "identities",
-		      "width=430,height=250,resizable=0,scrollbars=0,location=0");
-  w.opener = window;
-  w.focus();
-
-  preventDefault(event);
-}
-
-addEvent(window, 'load', initPreferences);
