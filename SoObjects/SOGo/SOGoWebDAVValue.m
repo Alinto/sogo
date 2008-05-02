5e9b6ce4194aebd515cfb03713b5d854945ebec0
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2008-05-02 22:47:21 +0000

    Monotone-Parent: c3c38efee718461f598c3836c1cbe39986c23fd2 Monotone-Revision: db028ce8c585be74840423effd3b77305f6ea051
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2008-05-02T22:47:21
    Monotone-Branch: ca.inverse.sogo

---
+++ SoObjects/SOGo/SOGoWebDAVValue.m
@@ -0,0 +1,37 @@
+/* SOGoWebDAVValue.m - this file is part of $PROJECT_NAME_HERE$
+ *
+ * Copyright (C) 2008 Inverse groupe conseil
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#import <Foundation/NSString.h>
+
+#import "SOGoWebDAVValue.h"
+
+@implementation SOGoWebDAVValue : SoWebDAVValue
+
+- (NSString *) stringForTag: (NSString *) _key
+                    rawName: (NSString *) setTag
+                  inContext: (id) context
+                   prefixes: (NSDictionary *) prefixes
+{
+  return object;
+}
+
+@end
