5eadf6a93691280e75277bc74ea88b6d753a5cc3
Francis Lachapelle <flachapelle@inverse.ca>
2008-09-02 20:52:09 +0000

    Fix for bug #2889
    Monotone-Parent: 2d32d54f3ab29efb615ba64c54fb4714a8cbd0a1
    Monotone-Revision: bf785cb3c555f8f6753cf915bc53455b1fb08004
    
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2008-09-02T20:52:09
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/MailerUI.js
+++ UI/WebServerResources/MailerUI.js
@@ -1567,8 +1567,12 @@ function getFoldersStateCallback(http) {
 function saveFoldersState() {
   if (mailAccounts.length > 0) {
     var foldersState = mailboxTree.getFoldersState();
-    var urlstr =  ApplicationBaseURL + "saveFoldersState" + "?expandedFolders=" + foldersState;
-    triggerAjaxRequest(urlstr, saveFoldersStateCallback);
+    var urlstr =  ApplicationBaseURL + "saveFoldersState";
+		var parameters = "expandedFolders=" + foldersState;
+    triggerAjaxRequest(urlstr, saveFoldersStateCallback, null, parameters,
+											 { "Content-type": "application/x-www-form-urlencoded",
+												 "Content-length": parameters.length,
+												 "Connection": "close" });
   }
 }
 
