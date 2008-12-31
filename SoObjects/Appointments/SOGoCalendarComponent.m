024a567d1af5e154cb00be795f4179afdcf9245a
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2008-12-31 02:57:54 +0000

    Monotone-Parent: 22aa1cec549d6f3ee5d911534369beaf41910226 Monotone-Revision: be94621e6c7d7ae0610925c213a0fc8f46eb30e9
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2008-12-31T02:57:54
    Monotone-Branch: ca.inverse.sogo

--- SoObjects/Appointments/SOGoCalendarComponent.m
+++ SoObjects/Appointments/SOGoCalendarComponent.m
@@ -430,7 +430,7 @@ _occurenceHasID (iCalRepeatableEntityObject *occurence, NSString *recID)
 	  [currentComponent removeAllExceptionDates];
 	  for (count = 0; count < max; count++)
 	    [currentComponent addToExceptionDates: [newDates objectAtIndex: count]];
-	}      
+	}
     }
 }
 
