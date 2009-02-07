2a7d8eec6527242281b690af5fafd018f40a750e
Francis Lachapelle <flachapelle@inverse.ca>
2009-02-07 00:18:34 +0000

    Added addressbook-home-set for principal URL
    Monotone-Parent: 911b87d5ac8f4df7258d042280cb3980cc12fe69
    Monotone-Revision: be11ede9b39c76d3a71c05143a448ab27a83a818
    
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2009-02-07T00:18:34
    Monotone-Branch: ca.inverse.sogo

---
+++ SoObjects/Contacts/SOGoUserFolder+Contacts.h
@@ -0,0 +1,37 @@
+/* SOGoUserFolder+Appointments.h - this file is part of SOGo
+ *
+ * Copyright (C) 2009 Inverse inc.
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef SOGOUSERFOLDER_CONTACTS_H
+#define SOGOUSERFOLDER_CONTACTS_H
+
+@class NSArray;
+@class NSString;
+
+#import <SOGo/SOGoUserFolder.h>
+
+@interface SOGoUserFolder (SOGoCalDAVSupport)
+
+- (NSArray *) davAddressbookHomeSet;
+
+@end
+
+#endif /* SOGOUSERFOLDER_CONTACTS_H */
