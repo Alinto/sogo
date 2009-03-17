70c0c0d919dae45acd247eb3f4aa85311c60de03
Francis Lachapelle <flachapelle@inverse.ca>
2009-03-17 19:38:05 +0000

    Fix for bug #3315
    Monotone-Parent: f3feeb51f538aeefcb5e90999e37b0bb069c3eba
    Monotone-Revision: b516b07191501368df1dfb16f25aee4b4fba7941
    
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2009-03-17T19:38:05
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/generic.js
+++ UI/WebServerResources/generic.js
@@ -698,7 +698,10 @@ function onMenuClickHandler(event) {
 function hideMenu(menuNode) {
 	var onHide;
 
-	if (menuNode && menuNode.submenu) {
+	if (!menuNode)
+		return;
+
+	if (menuNode.submenu) {
 		hideMenu(menuNode.submenu);
 		menuNode.submenu = null;
 	}
