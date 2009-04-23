f66e98f9bf8f5c9790e95b24162dc61337bb0218
Francis Lachapelle <flachapelle@inverse.ca>
2009-04-23 18:45:56 +0000

    Monotone-Parent: 9057f51730136ddbcf1d8e64c8029ea9a4e6c991 Monotone-Revision: b0266f0ed458f6976e3b9e50c36168d47b626a87
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2009-04-23T18:45:56
    Monotone-Branch: ca.inverse.sogo

---
+++ UI/WebServerResources/UIxReminderEditor.js
@@ -0,0 +1,42 @@
+/* -*- Mode: java; tab-width: 2; c-tab-always-indent: t; indent-tabs-mode: t; c-basic-offset: 2 -*- */
+function initializeWindowButtons() {
+  var okButton = $("okButton");
+  var cancelButton = $("cancelButton");
+
+  okButton.observe("click", onEditorOkClick, false);
+  cancelButton.observe("click", onEditorCancelClick, false);
+}
+
+
+function initializeFormValues() {
+  $("quantityField").value = parent$("reminderQuantity").value;
+	$("unitsList").value = parent$("reminderUnit").value;
+	$("relationsList").value = parent$("reminderRelation").value;
+	$("referencesList").value = parent$("reminderReference").value;
+}
+
+function onEditorOkClick(event) {
+  preventDefault(event);
+	if (parseInt($("quantityField").value) > 0) {
+		parent$("reminderQuantity").value = parseInt($("quantityField").value);
+		parent$("reminderUnit").value = $("unitsList").value;
+		parent$("reminderRelation").value = $("relationsList").value;
+		parent$("reminderReference").value = $("referencesList").value;
+		
+    window.close();
+	}
+	else
+		alert("heu");
+}
+
+function onEditorCancelClick(event) {
+  preventDefault(event);
+  window.close();
+}
+
+function onRecurrenceLoadHandler() {
+  initializeFormValues();
+  initializeWindowButtons();
+}
+
+document.observe("dom:loaded", onRecurrenceLoadHandler);
