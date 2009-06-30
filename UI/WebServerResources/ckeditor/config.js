3143aa9324e381b881493d18b67a20a0e11c7196
C Robert <crobert@inverse.ca>
2009-06-30 17:54:13 +0000

    Monotone-Parent: f0bf4f5e57bda2fe7cbbd558784c203b3fab4d36 Monotone-Revision: e8ee048170e91221cb7aac2c0e4e394db83bf2e2
    Monotone-Author: crobert@inverse.ca
    Monotone-Date: 2009-06-30T17:54:13
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/ckeditor/config.js
+++ UI/WebServerResources/ckeditor/config.js
@@ -8,4 +8,10 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	config.skin = 'kama';
+  //TODO: This should work to remove the bottom DOM information, but doesn't
+  // This way is on an instance of the config object
+  config.removePlugins = "elementspath,kplahj";
 };
+
+// This way is global / static
+CKEDITOR.config.removePlugins = "elementspath,kplahj";
