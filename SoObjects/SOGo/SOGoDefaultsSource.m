148ce32085d87c8afd55dcf5fbb5f1546d57dcc7
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2009-11-30 18:12:46 +0000

    Monotone-Parent: 7b560f5d25d1c37a7cdb773d653d58f82af95e42 Monotone-Revision: 30c81b4f13ebe8fe0c9c28588b0fca6366b0e3b2
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2009-11-30T18:12:46
    Monotone-Branch: ca.inverse.sogo

--- SoObjects/SOGo/SOGoDefaultsSource.m
+++ SoObjects/SOGo/SOGoDefaultsSource.m
@@ -123,8 +123,7 @@ NSString *SOGoDefaultsSourceUnmutableSource = @"SOGoDefaultsSourceUnmutableSourc
 - (void) setBool: (BOOL) value
 	  forKey: (NSString *) key
 {
-  [self setObject: [NSNumber numberWithBool: value]
-	forKey: key];
+  [self setInteger: (value) ? 1: 0 forKey: key];
 }
 
 - (BOOL) boolForKey: (NSString *) key
