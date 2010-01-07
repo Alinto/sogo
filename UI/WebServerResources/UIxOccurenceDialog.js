575ca49b2c5474ea8c1327cc592eca48d4dca1f4
Francis Lachapelle <flachapelle@inverse.ca>
2010-01-07 20:00:07 +0000

    Reindent
    Monotone-Parent: 0a5ce5307756135e2de40ddd71c96dce6f3f4e52
    Monotone-Revision: 18bcde63a97bfd2933b370687c939865d8c21d61
    
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2010-01-07T20:00:07
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/UIxOccurenceDialog.js
+++ UI/WebServerResources/UIxOccurenceDialog.js
@@ -1,42 +1,42 @@
 /* -*- Mode: java; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 function onCancelButtonClick(event) {
-  window.close();
+    window.close();
 }
 
 function onThisButtonClick(event) {
-  if (action == 'edit')
-    window.opener.performEventEdition(calendarFolder, componentName,
-																			recurrenceName);
-  else if (action == 'delete')
-    window.opener.performEventDeletion(calendarFolder, componentName,
-																			 recurrenceName);
-  else
-    window.alert("Invalid action: " + action);
+    if (action == 'edit')
+        window.opener.performEventEdition(calendarFolder, componentName,
+                                          recurrenceName);
+    else if (action == 'delete')
+        window.opener.performEventDeletion(calendarFolder, componentName,
+                                           recurrenceName);
+    else
+        window.alert("Invalid action: " + action);
 
-  window.close();
+    window.close();
 }
 
 function onAllButtonClick(event) {
-  if (action == 'edit')
-    window.opener.performEventEdition(calendarFolder, componentName);
-  else if (action == 'delete')
-    window.opener.performEventDeletion(calendarFolder, componentName);
-  else
-    window.alert("Invalid action: " + action);
+    if (action == 'edit')
+        window.opener.performEventEdition(calendarFolder, componentName);
+    else if (action == 'delete')
+        window.opener.performEventDeletion(calendarFolder, componentName);
+    else
+        window.alert("Invalid action: " + action);
 
-  window.close();
+    window.close();
 }
 
 function onOccurenceDialogLoad() {
-  var thisButton = $("thisButton");
-  thisButton.observe("click", onThisButtonClick);
+    var thisButton = $("thisButton");
+    thisButton.observe("click", onThisButtonClick);
 
-  var allButton = $("allButton");
-  allButton.observe("click", onAllButtonClick);
+    var allButton = $("allButton");
+    allButton.observe("click", onAllButtonClick);
 
-  var cancelButton = $("cancelButton");
-  cancelButton.observe("click", onCancelButtonClick);
+    var cancelButton = $("cancelButton");
+    cancelButton.observe("click", onCancelButtonClick);
 }
 
 document.observe("dom:loaded", onOccurenceDialogLoad);
