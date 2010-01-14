d46ac02ac12947d987119c037db8ab6e47beb04a
Francis Lachapelle <flachapelle@inverse.ca>
2010-01-14 16:35:33 +0000

    Update ckeditor to version 3.1
    Monotone-Parent: 6970e7a89b13f1353d770e663717021a53350856
    Monotone-Revision: 234b2f16dd5cc3d6eed2d10a0ab8fca81cb44795
    
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2010-01-14T16:35:33
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/ckeditor/config.js
+++ UI/WebServerResources/ckeditor/config.js
@@ -1,11 +1,10 @@
 ﻿/*
-Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 CKEDITOR.editorConfig = function( config )
 {
-  config.uiColor = '#D4D0C8';
-	config.skin = 'kama';
-  config.removePlugins = "elementspath,maximize,resize";
+    config.uiColor = '#D4D0C8';
+    config.removePlugins = "elementspath,maximize,resize";
 };
