0cc22a47e98b180dd77f7ccec85a50060f47975d
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2010-02-25 14:47:57 +0000

    Monotone-Parent: bc53d4486da9eb6b195273cef7d073fdcf53aa5c Monotone-Revision: 2ce9120e04a200ee236540135b63b57e1edac8c8
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2010-02-25T14:47:57
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/HTMLUListElement.js
+++
@@ -1,9 +0,0 @@
-/* -*- Mode: java; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-
-//HTMLUListElement.prototype.getSelectedRows = function() {
-//  return this.getSelectedNodes();
-//}
-//
-//HTMLUListElement.prototype.getSelectedRowsId = function() {
-//  return this.getSelectedNodesId();
-//}
