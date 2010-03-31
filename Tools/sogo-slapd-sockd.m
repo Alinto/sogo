b26a51234c32ed0410faaa447245d966f68011f9
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2010-03-31 20:16:40 +0000

    Monotone-Parent: 2f9e650d437ad6f3b4629d6df38c98b8151e4f0f Monotone-Revision: 6fa4b48fbe28f1cf7fd69a18c2504d3ac7c2e806
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2010-03-31T20:16:40
    Monotone-Branch: ca.inverse.sogo

---
+++ Tools/sogo-slapd-sockd.m
@@ -0,0 +1,63 @@
+/* sogo-slapd-sockd.m - this file is part of SOGo
+ *
+ * Copyright (C) 2010 Inverse inc.
+ *
+ * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSUserDefaults.h>
+
+#import "SOGoSockD.h"
+
+int
+main (int argc, char **argv, char **env)
+{
+  NSAutoreleasePool *pool;
+  NSUserDefaults *ud;
+  SOGoSockD *sockd;
+  int rc;
+
+  rc = 0;
+
+  pool = [NSAutoreleasePool new];
+
+  ud = [NSUserDefaults standardUserDefaults];
+  [ud addSuiteNamed: @"sogod"];
+
+  if ([ud objectForKey: @"SOGoUserSources"])
+    {
+      sockd = [SOGoSockD new];
+      if ([sockd run])
+        rc = 0;
+      else
+        rc = -1;
+      [sockd release];
+    }
+  else
+    {
+      NSLog (@"No LDAP source is configured in the SOGo configuration of this"
+             @" account. Please make sure to use this tool under the same"
+             @" username as SOGo.");
+      rc = -1;
+    }
+
+  [pool release];
+
+  return rc;
+}
