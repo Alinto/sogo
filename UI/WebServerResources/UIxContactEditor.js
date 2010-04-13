3b673c98644a1eae84195738be07ac55a12db26a
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2010-04-13 14:19:58 +0000

    reindentation
    Monotone-Parent: 51512f2a3102841c13942d3e4e41185e38cd9b5d
    Monotone-Revision: adc58f193001151889477351a8e1c5cce6a41ba1
    
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2010-04-13T14:19:58
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/UIxContactEditor.js
+++ UI/WebServerResources/UIxContactEditor.js
@@ -23,19 +23,19 @@
 */
 
 var uixEmailUsr = 
-  "([a-zA-Z0-9][a-zA-Z0-9_.-]*|\"([^\\\\\x80-\xff\015\012\"]|\\\\[^\x80-\xff])+\")";
+    "([a-zA-Z0-9][a-zA-Z0-9_.-]*|\"([^\\\\\x80-\xff\015\012\"]|\\\\[^\x80-\xff])+\")";
 var uixEmailDomain = 
-  "([a-zA-Z0-9][a-zA-Z0-9._-]*\\.)*[a-zA-Z0-9][a-zA-Z0-9._-]*\\.[a-zA-Z]{2,5}";
+    "([a-zA-Z0-9][a-zA-Z0-9._-]*\\.)*[a-zA-Z0-9][a-zA-Z0-9._-]*\\.[a-zA-Z]{2,5}";
 var uixEmailRegex = new RegExp("^"+uixEmailUsr+"\@"+uixEmailDomain+"$");
 
 var displayNameChanged = false;
 
 function unescapeCallbackParameter(s) {
-  if(!s || s.length == 0)
+    if(!s || s.length == 0)
+        return s;
+    s = s.replace(/&apos;/g, "'");
+    s = s.replace(/&quot;/g, '"');
     return s;
-  s = s.replace(/&apos;/g, "'");
-  s = s.replace(/&quot;/g, '"');
-  return s;
 }
 
 function copyContact(type, email, uid, sn,
@@ -43,87 +43,85 @@ function copyContact(type, email, uid, sn,
 										 mobile, postalAddress, homePostalAddress, 
 										 departmentNumber, l)
 {
-	//  var type = arguments[0]; 
-	//  var email = arguments[1]; 
-	//  var uid = arguments[2]; 
-	//  var sn = arguments[3]; 
-	//  var givenName = arguments[4]; 
-	//  var telephoneNumber = arguments[5]; 
-	//  var facsimileTelephoneNumber = arguments[6]; 
-	//  var mobile = arguments[7]; 
-	//  var postalAddress = arguments[8]; 
-	//  var homePostalAddress = arguments[9]; 
-	//  var departmentNumber = arguments[10]; 
-	//  var l = arguments[11]; 
-  var e;
-  e = $('cn');
-  e.setAttribute('value', unescapeCallbackParameter(cn));
-  e = $('email');
-  e.setAttribute('value', email);
-  e = $('sn');
-  e.setAttribute('value', unescapeCallbackParameter(sn));
-  e = $('givenName');
-  e.setAttribute('value', unescapeCallbackParameter(givenName));
-  e = $('telephoneNumber');
-  e.setAttribute('value', telephoneNumber);
-  e = $('facsimileTelephoneNumber');
-  e.setAttribute('value', facsimileTelephoneNumber);
-  e = $('mobile');
-  e.setAttribute('value', mobile);
-  e = $('postalAddress');
-  e.setAttribute('value', unescapeCallbackParameter(postalAddress));
-  e = $('homePostalAddress');
-  e.setAttribute('value', unescapeCallbackParameter(homePostalAddress));
-  e = $('departmentNumber');
-  e.setAttribute('value', unescapeCallbackParameter(departmentNumber));
-  e = $('l');
-  e.setAttribute('value', unescapeCallbackParameter(l));
+    //  var type = arguments[0]; 
+    //  var email = arguments[1]; 
+    //  var uid = arguments[2]; 
+    //  var sn = arguments[3]; 
+    //  var givenName = arguments[4]; 
+    //  var telephoneNumber = arguments[5]; 
+    //  var facsimileTelephoneNumber = arguments[6]; 
+    //  var mobile = arguments[7]; 
+    //  var postalAddress = arguments[8]; 
+    //  var homePostalAddress = arguments[9]; 
+    //  var departmentNumber = arguments[10]; 
+    //  var l = arguments[11]; 
+    var e;
+    e = $('cn');
+    e.setAttribute('value', unescapeCallbackParameter(cn));
+    e = $('email');
+    e.setAttribute('value', email);
+    e = $('sn');
+    e.setAttribute('value', unescapeCallbackParameter(sn));
+    e = $('givenName');
+    e.setAttribute('value', unescapeCallbackParameter(givenName));
+    e = $('telephoneNumber');
+    e.setAttribute('value', telephoneNumber);
+    e = $('facsimileTelephoneNumber');
+    e.setAttribute('value', facsimileTelephoneNumber);
+    e = $('mobile');
+    e.setAttribute('value', mobile);
+    e = $('postalAddress');
+    e.setAttribute('value', unescapeCallbackParameter(postalAddress));
+    e = $('homePostalAddress');
+    e.setAttribute('value', unescapeCallbackParameter(homePostalAddress));
+    e = $('departmentNumber');
+    e.setAttribute('value', unescapeCallbackParameter(departmentNumber));
+    e = $('l');
+    e.setAttribute('value', unescapeCallbackParameter(l));
 };
 
 function validateContactEditor() {
-  var rc = true;
+    var rc = true;
   
-  var e = $('workMail');
-  if (e.value.length > 0
-      && !uixEmailRegex.test(e.value))
-    {
-      alert(getLabel ("invalidemailwarn"));
-      rc = false;
+    var e = $('workMail');
+    if (e.value.length > 0
+        && !uixEmailRegex.test(e.value)) {
+        alert(getLabel ("invalidemailwarn"));
+        rc = false;
     }
 
-  e = $('homeMail');
-  if (e.value.length > 0
-      && !uixEmailRegex.test(e.value))
-    {
-      alert(getLabel ("invalidemailwarn"));
-      rc = false;
+    e = $('homeMail');
+    if (e.value.length > 0
+        && !uixEmailRegex.test(e.value)) {
+        alert(getLabel ("invalidemailwarn"));
+        rc = false;
     }
 
-  return rc;
+    return rc;
 }
 
 function onFnKeyDown() {
-  var fn = $("fn");
-  fn.onkeydown = null;
-  displayNameChanged = true;
+    var fn = $("fn");
+    fn.onkeydown = null;
+    displayNameChanged = true;
 
-  return true;
+    return true;
 }
 
 function onFnNewValue(event) {
-  if (!displayNameChanged) {
-    var sn = $("sn").value.trim();
-    var givenName = $("givenName").value.trim();
+    if (!displayNameChanged) {
+        var sn = $("sn").value.trim();
+        var givenName = $("givenName").value.trim();
 
-    var fullName = givenName;
-    if (fullName && sn)
-      fullName += ' ';
-    fullName += sn;
+        var fullName = givenName;
+        if (fullName && sn)
+            fullName += ' ';
+        fullName += sn;
 
-    $("fn").value = fullName;
-  }
+        $("fn").value = fullName;
+    }
 
-  return true;
+    return true;
 }
 
 function onEditorCancelClick(event) {
@@ -133,9 +131,9 @@ function onEditorCancelClick(event) {
 }
 
 function onEditorSubmitClick(event) {
-  if (validateContactEditor())
-      $('mainForm').submit();
-  this.blur();
+    if (validateContactEditor())
+        $('mainForm').submit();
+    this.blur();
 }
 
 function initEditorForm() {
@@ -143,13 +141,13 @@ function initEditorForm() {
     var controller = new SOGoTabsController();
     controller.attachToTabsContainer(tabsContainer);
 
-  displayNameChanged = ($("fn").value.length > 0);
-  $("fn").onkeydown = onFnKeyDown;
-  $("sn").onkeyup = onFnNewValue;
-  $("givenName").onkeyup = onFnNewValue;
+    displayNameChanged = ($("fn").value.length > 0);
+    $("fn").onkeydown = onFnKeyDown;
+    $("sn").onkeyup = onFnNewValue;
+    $("givenName").onkeyup = onFnNewValue;
 
-  $("cancelButton").observe("click", onEditorCancelClick);
-  $("submitButton").observe("click", onEditorSubmitClick);
+    $("cancelButton").observe("click", onEditorCancelClick);
+    $("submitButton").observe("click", onEditorSubmitClick);
 }
 
 document.observe("dom:loaded", initEditorForm);
