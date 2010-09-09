d67826dcd06625e8406c0865373b698faeb34b7c
Ludovic Marcotte <lmarcotte@inverse.ca>
2010-09-09 17:03:27 +0000

    See ChangeLog
    Monotone-Parent: e97646218dfeb52609d77419b9e445c6e36c0ad0
    Monotone-Revision: 4bf3403d9b8b5ffb0f071863663473bb6c30c47f
    
    Monotone-Author: ludovic@Sophos.ca
    Monotone-Date: 2010-09-09T17:03:27
    Monotone-Branch: ca.inverse.sogo

--- Main/NSException+Stacktrace.h
+++ Main/NSException+Stacktrace.h
@@ -1,32 +1,24 @@
-/** Interface for NSException for GNUStep
-   Copyright (C) 1995, 1996 Free Software Foundation, Inc.
-
-   Written by:  Adam Fedor <fedor@boulder.colorado.edu>
-   Date: 1995
-   
-   This file is part of the GNUstep Base Library.
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-   
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-   
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02111 USA.
-
-    <title>NSException and NSAssertionHandler class reference</title>
-
-    AutogsdocSource: NSAssertionHandler.m
-    AutogsdocSource: NSException.m
-
-*/
+/* NSException+Stacktrace.h - this file is part of SOGo
+ *
+ * Copyright (C) 2010 Inverse inc.
+ *
+ * Author: Ludovic Marcotte <lmarcotte@inverse.ca>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #import <Foundation/NSException.h>
 
