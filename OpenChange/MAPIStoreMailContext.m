de1a3cbbf75b907e78ed182f02e5fa6d06ae0b27
Wolfgang Sourdeau <wsourdeau@inverse.ca>
2010-10-13 15:45:36 +0000

    Monotone-Parent: 72b29691e36019f10738562fa27dc533a1b1adce Monotone-Revision: 070788cd67236498278b572e2fac73279fe07d2d
    Monotone-Author: wsourdeau@inverse.ca
    Monotone-Date: 2010-10-13T15:45:36
    Monotone-Branch: ca.inverse.sogo

--- OpenChange/MAPIStoreMailContext.m
+++ OpenChange/MAPIStoreMailContext.m
@@ -119,6 +119,7 @@ static Class SOGoUserFolderK;
       /* read mail, see http://msdn.microsoft.com/en-us/library/cc815472.aspx */
       *data = MAPILongValue (memCtx, 0x00000100);
       break;
+    case PR_CONVERSATION_TOPIC:
     case PR_SUBJECT_UNICODE:
       child = [self lookupObject: childURL];
       *data = [[child decodedSubject] asUnicodeInMemCtx: memCtx];
@@ -134,29 +135,47 @@ static Class SOGoUserFolderK;
       offsetDate = [[child date] addYear: -1 month: 0 day: 0
                                     hour: 0 minute: 0 second: 0];
       *data = [offsetDate asFileTimeInMemCtx: memCtx];
+      // *data = [[child date] asFileTimeInMemCtx: memCtx];
       break;
-    case PR_FLAG_STATUS: // TODO
-    case PR_MSG_STATUS: // TODO
     case PR_MESSAGE_FLAGS: // TODO
+      // NSDictionary *coreInfos;
+      // NSArray *flags;
+      // child = [self lookupObject: childURL];
+      // coreInfos = [child fetchCoreInfos];
+      // flags = [coreInfos objectForKey: @"flags"];
+      *data = MAPILongValue (memCtx, 0x02 | 0x20); // fromme + unmodified
+      break;
+
+    case PR_FLAG_STATUS: // TODO
     case PR_SENSITIVITY: // TODO
+    case PR_FOLLOWUP_ICON: // TODO
       *data = MAPILongValue (memCtx, 0);
       break;
-    case PR_IMPORTANCE: // TODO
-      *data = MAPILongValue (memCtx, 1);
+
+    case PR_EXPIRY_TIME: // TODO
+    case PR_REPLY_TIME:
+      *data = [[NSCalendarDate date] asFileTimeInMemCtx: memCtx];
       break;
-    case PR_MESSAGE_SIZE: // TODO
-      child = [self lookupObject: childURL];
-      /* TODO: choose another name for that method */
-      *data = MAPILongValue (memCtx, [[child davContentLength] intValue]);
-      break;
-      // #define PR_REPLY_TIME                                       PROP_TAG(PT_SYSTIME   , 0x0030) /* 0x00300040 */
-      // #define PR_EXPIRY_TIME                                      PROP_TAG(PT_SYSTIME   , 0x0015) /* 0x00150040 */
-      // #define PR_MESSAGE_DELIVERY_TIME                            PROP_TAG(PT_SYSTIME   , 0x0e06) /* 0x0e060040 */
-      // #define PR_FOLLOWUP_ICON                                    PROP_TAG(PT_LONG      , 0x1095) /* 0x10950003 */
+      
+
       // #define PR_ITEM_TEMPORARY_FLAGS                             PROP_TAG(PT_LONG      , 0x1097) /* 0x10970003 */
       // #define PR_SEARCH_KEY                                       PROP_TAG(PT_BINARY    , 0x300b) /* 0x300b0102 */
 
+      // 36030003 - PR_CONTENT_UNREAD
+      // 36020003 - PR_CONTENT_COUNT
+      // 10970003 - 
+      // 81f80003
+      // 10950003
+      // 819d0003
+      // 300b0102
+      // 81fa000b
+      // 00300040
+      // 00150040
 
+    case PR_RECEIVED_BY_NAME:
+      child = [self lookupObject: childURL];
+      *data = [[child to] asUnicodeInMemCtx: memCtx];
+      break;
 
     case PR_SENT_REPRESENTING_NAME_UNICODE:
       child = [self lookupObject: childURL];
@@ -166,6 +185,10 @@ static Class SOGoUserFolderK;
       child = [self lookupObject: childURL];
       *data = [[child messageId] asUnicodeInMemCtx: memCtx];
       break;
+
+    case PR_READ_RECEIPT_REQUESTED: // TODO
+      *data = MAPIBoolValue (memCtx, NO);
+
     default:
       rc = [super getMessageTableChildproperty: data
                                          atURL: childURL
