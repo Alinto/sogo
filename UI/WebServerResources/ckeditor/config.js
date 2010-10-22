05ddd1a42d33ab03d26e384bcd3a1b9be9bf5736
Francis Lachapelle <flachapelle@inverse.ca>
2010-10-22 20:32:30 +0000

    Updated CKEditor to version 3.4.1. Also enabled the spell checker by default.
    Monotone-Parent: 56c04a536789d0ff8728e96ba89596c9a3c2bdea
    Monotone-Revision: 48315db3a4e6bc9edd9f075ada5c9264cac4297e
    
    Monotone-Author: flachapelle@inverse.ca
    Monotone-Date: 2010-10-22T20:32:30
    Monotone-Branch: ca.inverse.sogo

--- UI/WebServerResources/ckeditor/config.js
+++ UI/WebServerResources/ckeditor/config.js
@@ -6,5 +6,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.editorConfig = function( config )
 {
     config.uiColor = '#D4D0C8';
+    config.skin = 'kama';
     config.removePlugins = "elementspath,maximize,resize";
+    config.scayt_autoStartup = true;
 };
