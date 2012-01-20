f16cefad3190c5d26a912638a24ca832dd94e7e1
Jean Raby <jraby@inverse.ca>
2012-01-20 18:43:29 +0000

    Update the sql scripts to add a primary key to the OCSSessionFolder table.
    Monotone-Parent: e13dda2a639bb68b9e67d7b4dd75235d9d98ef1b
    Monotone-Revision: ecc0c907192778ee08a07fb020aa037de83d7bed
    
    Monotone-Author: jraby@inverse.ca
    Monotone-Date: 2012-01-20T18:43:29

--- Scripts/sql-update-1.3.11_to_1.3.12.sh
+++ Scripts/sql-update-1.3.11_to_1.3.12.sh
@@ -34,6 +34,7 @@ function convVCtoText() {
     IFS="$oldIFS"
 }
 
+echo "Step 1 - Converting c_content from VARCHAR to TEXT in table sogo_folder_info" >&2
 tables=`psql -t -U $username -h $hostname $database -c "select split_part(c_location, '/', 5) from $indextable;"`
 
 for table in $tables;
@@ -42,3 +43,13 @@ do
 done
 
 echo "$sqlscript" | psql -q -e -U $username -h $hostname $database
+
+echo "Step 2 - Fix primary key for the sogo_sessions table" >&2
+SESSIONTBL=$(su - sogo -c "defaults read sogod OCSSessionsFolderURL" | awk -F/ '{print $NF}')
+if [ -z "$SESSIONTBL" ]; then
+  echo "Failed to obtain session table name" >&2
+  exit 1
+fi
+
+psql -e -U $username -h $hostname $database -c "ALTER TABLE $SESSIONTBL ADD PRIMARY KEY (c_id);"
+
