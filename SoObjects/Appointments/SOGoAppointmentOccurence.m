63560c567883e6b19ab078e7a1e8c7d39eaca7fb
Ludovic Marcotte <lmarcotte@inverse.ca>
2012-04-23 12:58:56 +0000

    See ChangeLog
    Monotone-Parent: e290d09c6fe813e82f2e06c1cfcaab7ba2a5b3ac
    Monotone-Revision: 64488d85ff930e32a07eda3c4ce9ae01e7513fc8
    
    Monotone-Author: ludovic@Sophos.ca
    Monotone-Date: 2012-04-23T12:58:56

--- SoObjects/Appointments/SOGoAppointmentOccurence.m
+++ SoObjects/Appointments/SOGoAppointmentOccurence.m
@@ -1,6 +1,6 @@
 /* SOGoAppointmentOccurence.m - this file is part of SOGo
  * 
- * Copyright (C) 2008 Inverse inc.
+ * Copyright (C) 2008-2012 Inverse inc.
  *
  * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
  *
@@ -24,4 +24,8 @@
 
 @implementation SOGoAppointmentOccurence
 
+- (NSString *) componentTag
+{ 
+  return [container componentTag];
+}
 @end
