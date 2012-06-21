6d745e0f4b07236d0ede4a6012d776264240290c
Ludovic Marcotte <lmarcotte@inverse.ca>
2012-06-21 23:32:18 +0000

    See ChangeLog
    Monotone-Parent: aadc4759a64f724760d38bbdfd92715182856439
    Monotone-Revision: 5e99e6ac2d5684eba786f2ce14295f3a3b5267a3
    
    Monotone-Author: ludovic@Sophos.ca
    Monotone-Date: 2012-06-21T23:32:18

--- UI/Contacts/UIxContactsFilterPanel.m
+++ UI/Contacts/UIxContactsFilterPanel.m
@@ -1,14 +1,15 @@
 /*
  Copyright (C) 2000-2005 SKYRIX Software AG
+ Copyright (C) 2000-2012 Inverse inc.
  
- This file is part of OpenGroupware.org.
+ This file is part of SOGo.
  
- OGo is free software; you can redistribute it and/or modify it under
+ SOGo is free software; you can redistribute it and/or modify it under
  the terms of the GNU Lesser General Public License as published by the
  Free Software Foundation; either version 2, or (at your option) any
  later version.
  
- OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+ SOGo is distributed in the hope that it will be useful, but WITHOUT ANY
  WARRANTY; without even the implied warranty of MERCHANTABILITY or
  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
  License for more details.
