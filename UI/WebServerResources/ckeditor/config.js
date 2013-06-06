5d6a5f5d5ed13be922226f2894ca8eed57f5061b
Francis Lachapelle <flachapelle@inverse.ca>
2013-06-06 13:21:26 -0400

    Update CKEditor to version 4.1.1

--- UI/WebServerResources/ckeditor/config.js
+++ UI/WebServerResources/ckeditor/config.js
@@ -1,19 +1,41 @@
 /**
- * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
+    config.toolbar = [
+        { name: 'basicstyles', items: [ 'Bold', 'Italic', 'TextColor' ] },
+        { name: 'paragraph', items: [ 'NumberedList', 'BulletedList',
+                                      '-',
+                                      'Outdent', 'Indent',
+                                      '-',
+                                      'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
+        { name: 'links', items: [ 'Link', 'Unlink' ] },
+        { name: 'insert', items: [ 'Image' ] },
+        { name: 'editing', items: [ 'Font', 'FontSize', 'Scayt' ] }
+    ];
     config.toolbarGroups = [
-        [ 'Bold', 'Italic', 'TextColor' ],
-        ['Font', 'FontSize'],
-	{ name: 'paragraph',   groups: [ 'list', 'indent', 'align' ] },
+        { name: 'basicstyles' },
+	{ name: 'paragraph' },
 	{ name: 'links' },
 	{ name: 'insert' },
-        ['SpellChecker','Scayt']
+        { name: 'editing' }
     ];
 
-    config.removeButtons = 'Anchor,Underline,Strike,Subscript,Superscript';
-    config.removeDialogTabs = 'link:advanced';
+    config.removeDialogTabs = 'link:advanced;image:advanced';
     config.enterMode = CKEDITOR.ENTER_BR;
+
+    // Disables the built-in words spell checker if browser provides one. Defaults to true.
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config-cfg-disableNativeSpellChecker
+    //config.disableNativeSpellChecker = false;
+
+    // Whether to show the browser native context menu when the Ctrl or Meta (Mac) key is pressed on opening the context
+    // menu with the right mouse button click or the Menu key. Defaults to true.
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config-cfg-browserContextMenuOnCtrl
+    //config.browserContextMenuOnCtrl = false;
+
+    // If enabled, turns on SCAYT automatically after loading the editor. Defaults to false.
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config-cfg-scayt_autoStartup
+    //config.scayt_autoStartup = true;
 };
