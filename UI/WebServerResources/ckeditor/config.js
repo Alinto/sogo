37de8c6141a04fdd76f5d1b439f937aab1d2ec0b
Francis Lachapelle <flachapelle@inverse.ca>
2013-12-02 13:41:39 -0500

    CKEditor: don't filter tags

--- UI/WebServerResources/ckeditor/config.js
+++ UI/WebServerResources/ckeditor/config.js
@@ -20,6 +20,7 @@ CKEDITOR.editorConfig = function( config ) {
     config.removeDialogTabs = 'link:advanced;image:advanced';
     config.enterMode = CKEDITOR.ENTER_BR;
     config.tabSpaces = 4;
+    config.allowedContent = true; // don't filter tags
 
     // Disables the built-in words spell checker if browser provides one. Defaults to true.
     // http://docs.ckeditor.com/#!/api/CKEDITOR.config-cfg-disableNativeSpellChecker
