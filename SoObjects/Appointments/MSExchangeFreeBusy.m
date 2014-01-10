a24d809e4061056be6082b310bae1b9d8edb6883
Ludovic Marcotte <lmarcotte@inverse.ca>
2014-01-09 21:06:31 -0500

    Minor code/copyright/authors cleanups

--- SoObjects/Appointments/MSExchangeFreeBusy.m
+++ SoObjects/Appointments/MSExchangeFreeBusy.m
@@ -1,8 +1,6 @@
 /* MSExchangeFreeBusy.m - this file is part of SOGo
  *
- * Copyright (C) 2012 Inverse inc.
- *
- * Author: Francis Lachapelle <flachapelle@inverse.ca>
+ * Copyright (C) 2012-2014 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- UI/Contacts/UIxContactView.m
+++ UI/Contacts/UIxContactView.m
@@ -1,6 +1,6 @@
 /*
   Copyright (C) 2004 SKYRIX Software AG
-  Copyright (C) 2005-2012 Inverse inc.
+  Copyright (C) 2005-2014 Inverse inc.
 
   This file is part of SOGo.
  
