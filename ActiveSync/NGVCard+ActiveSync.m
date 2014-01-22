e7f38f940ea02849b6fb8b1a2898066181969495
Ludovic Marcotte <lmarcotte@inverse.ca>
2014-01-22 11:27:27 -0500

    Moved the cards' logic into a category and added support for more AS ones

--- ActiveSync/NGVCard+ActiveSync.m
+++ ActiveSync/NGVCard+ActiveSync.m
@@ -35,24 +35,124 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import <NGCards/CardElement.h>
 
+#import <Contacts/NGVCard+SOGo.h>
+
 @implementation NGVCard (ActiveSync)
 
 - (NSString *) activeSyncRepresentation
 {
-  NSString *firstName, *lastName;
+  CardElement *n, *homeAdr, *workAdr;
   NSMutableString *s;
-  CardElement *n;
+  NSArray *emails;
+  id o;
+
+  int i;
 
   s = [NSMutableString string];
   n = [self n];
   
-  lastName = [n flattenedValueAtIndex: 0 forKey: @""];
-  [s appendFormat: @"<LastName xmlns=\"Contacts:\">%@</LastName>", lastName];
+  if ((o = [n flattenedValueAtIndex: 0 forKey: @""]))
+    [s appendFormat: @"<LastName xmlns=\"Contacts:\">%@</LastName>", o];
   
+  if ((o = [n flattenedValueAtIndex: 1 forKey: @""]))
+    [s appendFormat: @"<FirstName xmlns=\"Contacts:\">%@</FirstName>", o];
   
-  firstName = [n flattenedValueAtIndex: 1 forKey: @""];
-  [s appendFormat: @"<FirstName xmlns=\"Contacts:\">%@</FirstName>", firstName];
+  if ((o = [self workCompany]))
+    [s appendFormat: @"<CompanyName xmlns=\"Contacts:\">%@</CompanyName>", o];
+  
+  if ((o = [self title]))
+    [s appendFormat: @"<JobTitle xmlns=\"Contacts:\">%@</JobTitle>", o];
 
+  if ((o = [self preferredEMail]))    [s appendFormat: @"<HomePhoneNumber xmlns=\"Contacts:\">%@</HomePhoneNumber>", o];
+
+    [s appendFormat: @"<Email1Address xmlns=\"Contacts:\">%@</Email1Address>", o];    [s appendFormat: @"<HomePhoneNumber xmlns=\"Contacts:\">%@</HomePhoneNumber>", o];
+    [s appendFormat: @"<HomePhoneNumber xmlns=\"Contacts:\">%@</HomePhoneNumber>", o];
+
+  
+  // Secondary email addresses
+  emails = [self secondaryEmails];
+    [s appendFormat: @"<HomePhoneNumber xmlns=\"Contacts:\">%@</HomePhoneNumber>", o];
+
+  for (i = 0; i < [emails count]; i++)
+    {
+      o = [[emails objectAtIndex: i] flattenedValuesForKey: @""];
+      
+      [s appendFormat: @"<Email%dAddress xmlns=\"Contacts:\">%@</Email%dAddress>", i+2, o, i+2];
+
+      if (i == 1)
+        break;
+    }
+
+  // Telephone numbers
+  if ((o = [self workPhone]))
+    [s appendFormat: @"<BusinessPhoneNumber xmlns=\"Contacts:\">%@</BusinessPhoneNumber>", o];
+  
+  if ((o = [self homePhone]))
+    [s appendFormat: @"<HomePhoneNumber xmlns=\"Contacts:\">%@</HomePhoneNumber>", o];
+  
+  if ((o = [self fax]))
+    [s appendFormat: @"<BusinessFaxNumber xmlns=\"Contacts:\">%@</BusinessFaxNumber>", o];
+  
+  if ((o = [self mobile]))
+    [s appendFormat: @"<MobilePhoneNumber xmlns=\"Contacts:\">%@</MobilePhoneNumber>", o];
+  
+  if ((o = [self pager]))
+    [s appendFormat: @"<PagerNumber xmlns=\"Contacts:\">%@</PagerNumber>", o];
+
+  // Home Address
+  homeAdr = [[self childrenWithTag: @"adr"
+                      andAttribute: @"type"
+                       havingValue: @"home"] objectAtIndex: 0];
+
+  if ((o = [homeAdr flattenedValueAtIndex: 2  forKey: @""]))
+    [s appendFormat: @"<HomeStreet xmlns=\"Contacts:\">%@</HomeStreet>", o];
+  
+  if ((o = [homeAdr flattenedValueAtIndex: 3  forKey: @""]))
+    [s appendFormat: @"<HomeCity xmlns=\"Contacts:\">%@</HomeCity>", o];
+  
+  if ((o = [homeAdr flattenedValueAtIndex: 4  forKey: @""]))
+    [s appendFormat: @"<HomeState xmlns=\"Contacts:\">%@</HomeState>", o];
+  
+  if ((o = [homeAdr flattenedValueAtIndex: 5  forKey: @""]))
+    [s appendFormat: @"<HomePostalCode xmlns=\"Contacts:\">%@</HomePostalCode>", o];
+  
+  if ((o = [homeAdr flattenedValueAtIndex: 6  forKey: @""]))
+    [s appendFormat: @"<HomeCountry xmlns=\"Contacts:\">%@</HomeCountry>", o];
+
+  // Work Address
+  workAdr = [[self childrenWithTag: @"adr"
+                      andAttribute: @"type"
+                       havingValue: @"work"] objectAtIndex: 0];
+  
+  if ((o = [workAdr flattenedValueAtIndex: 2  forKey: @""]))
+    [s appendFormat: @"<BusinessStreet xmlns=\"Contacts:\">%@</BusinessStreet>", o];
+  
+  if ((o = [workAdr flattenedValueAtIndex: 3  forKey: @""]))
+    [s appendFormat: @"<BusinessCity xmlns=\"Contacts:\">%@</BusinessCity>", o];
+  
+  if ((o = [workAdr flattenedValueAtIndex: 4  forKey: @""]))
+    [s appendFormat: @"<BusinessState xmlns=\"Contacts:\">%@</BusinessState>", o];
+  
+  if ((o = [workAdr flattenedValueAtIndex: 5  forKey: @""]))
+    [s appendFormat: @"<BusinessPostalCode xmlns=\"Contacts:\">%@</BusinessPostalCode>", o];
+  
+  if ((o = [workAdr flattenedValueAtIndex: 6  forKey: @""]))
+    [s appendFormat: @"<BusinessCountry xmlns=\"Contacts:\">%@</BusinessCountry>", o];
+
+  // Other, less important fields
+  if ((o = [self birthday]))
+    [s appendFormat: @"<Birthday xmlns=\"Contacts:\">%@</Birthday>", [o activeSyncRepresentation]];
+
+  if ((o = [self note]))
+    {
+      [s appendString: @"<Body xmlns=\"AirSyncBase:\">"];
+      [s appendFormat: @"<Type>%d</Type>", 1]; 
+      [s appendFormat: @"<EstimatedDataSize>%d</EstimatedDataSize>", [o length]];
+      [s appendFormat: @"<Truncated>%d</Truncated>", 0];
+      [s appendFormat: @"<Data>%@</Data>", o];
+      [s appendString: @"</Body>"];
+    }
+  
   return s;
 }
 
