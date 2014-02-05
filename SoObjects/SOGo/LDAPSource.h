3950e9d4c33417cbf821f5554e566e6a68b7d0b0
Ludovic Marcotte <lmarcotte@inverse.ca>
2014-02-04 21:03:11 -0500

    Moved DN cache to SOGoCache

--- SoObjects/SOGo/LDAPSource.h
+++ SoObjects/SOGo/LDAPSource.h
@@ -1,6 +1,6 @@
 /* LDAPSource.h - this file is part of SOGo
  *
- * Copyright (C) 2007-2013 Inverse inc.
+ * Copyright (C) 2007-2014 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -78,8 +78,6 @@
   NSMutableArray *searchAttributes;
   
   BOOL passwordPolicy;
-
-  NSMutableDictionary *_dnCache;
 
   /* resources handling */
   NSString *kindField;
