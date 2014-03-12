c8550c8a4a8f0c0e5a1bb7c02e3e891506b3ca37
Francis Lachapelle <flachapelle@inverse.ca>
2014-03-12 16:35:22 -0400

    Fix copy/move to folder with numerical prefix
    Fixes #2658

--- UI/WebServerResources/MailerUI.js
+++ UI/WebServerResources/MailerUI.js
@@ -2857,7 +2857,7 @@ function Mailbox(type, name, unseen, displayName) {
     else
       this.displayName = name;
     // log("name: " + name + "; dn: " + displayName);
-    this.name = name.asCSSIdentifier();
+    this.name = name;
     this.unseen = unseen;
     this.parentFolder = null;
     this.children = new Array();
@@ -2878,7 +2878,7 @@ Mailbox.prototype = {
 
         var currentFolder = this;
         while (currentFolder.parentFolder) {
-            fullName = "/folder" + currentFolder.name + fullName;
+            fullName = ("/folder" + currentFolder.name).asCSSIdentifier() + fullName;
             currentFolder = currentFolder.parentFolder;
         }
 
