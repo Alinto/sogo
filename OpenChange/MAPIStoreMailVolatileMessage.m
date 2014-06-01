4b383705fff833a1c41dde513119fe847bbaefc7
Kamen Mazdrashki <kmazdrashki@zentyal.com>
2014-06-02 01:18:23 +0200

    openchange: Fix submitWithFlags to submit to BCC-ed recipients also
    this reintroduces commit 7efb263c659451d2c781547a5b38cb5d01ed837f
    
    Signed-off-by: Kamen Mazdrashki <kmazdrashki@zentyal.com>

--- OpenChange/MAPIStoreMailVolatileMessage.m
+++ OpenChange/MAPIStoreMailVolatileMessage.m
@@ -865,7 +865,7 @@ MakeMessageBody (NSDictionary *mailProperties, NSDictionary *attachmentParts, NS
   NSMutableArray *recipientEmails;
   NSArray *list;
   NSString *recId, *from, *msgClass;
-  NSUInteger count;
+  enum ulRecipClass type;
   SOGoUser *activeUser;
   SOGoDomainDefaults *dd;
   NSException *error;
@@ -881,9 +881,9 @@ MakeMessageBody (NSDictionary *mailProperties, NSDictionary *attachmentParts, NS
       
       recipientEmails = [NSMutableArray arrayWithCapacity: 32];
       recipients = [properties objectForKey: @"recipients"];
-      for (count = 0; count < 3; count++)
+      for (type = MAPI_ORIG; type <= MAPI_BCC; type++)
         {
-          recId = recTypes[count];
+          recId = recTypes[type];
           list = [recipients objectForKey: recId];
           [recipientEmails
             addObjectsFromArray: [list objectsForKey: @"email"
