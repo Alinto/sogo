c5c2bfb7a6117b00d563bf0230fb435b6026972d
Ludovic Marcotte <lmarcotte@inverse.ca>
2014-09-15 14:08:54 -0400

    Fixed iCal detection for 10.9 (and perhaps even 10.8)

--- SoObjects/Appointments/SOGoUserFolder+Appointments.h
+++ SoObjects/Appointments/SOGoUserFolder+Appointments.h
@@ -1,8 +1,6 @@
 /* SOGoUserFolder+Appointments.h - this file is part of SOGo
  *
- * Copyright (C) 2008-2010 Inverse inc.
- *
- * Author: Wolfgang Sourdeau <wsourdeau@inverse.ca>
+ * Copyright (C) 2008-2014 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
