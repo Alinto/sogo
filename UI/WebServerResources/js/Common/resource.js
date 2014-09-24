342441ecf819f53c81e6a2ceb2b9658cf212637a
Francis Lachapelle <flachapelle@inverse.ca>
2014-09-24 16:06:12 -0400

    Improve JavaScript coding style
    To comitted code now passes the Airbnb pattern using jscs

--- UI/WebServerResources/js/Common/resource.js
+++ UI/WebServerResources/js/Common/resource.js
@@ -1,109 +1,111 @@
+/* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
 (function() {
-    'use strict';
+  'use strict';
 
-    /* Constructor  */
-    function Resource($http, $q, path, options) {
-        angular.extend(this, {
-            _http: $http,
-            _q: $q,
-            _path: path
-        });
-        angular.extend(this, options);
-    }
+  /* Constructor  */
+  function Resource($http, $q, path, options) {
+    angular.extend(this, {
+      _http: $http,
+      _q: $q,
+      _path: path
+    });
+    angular.extend(this, options);
+  }
 
-    /* The factory we'll use to register with Angular */
-    Resource.$factory =  ['$http', '$q', function($http, $q) {
-        return function(path, options) {
-            return new Resource($http, $q, path, options);
-        };
-    }];
-
-    /* Factory registration in Angular module */
-    angular.module('SOGo.Common').factory('sgResource', Resource.$factory);
-
-    /* Instance methods */
-
-    Resource.prototype.path = function(uid) {
-        return (uid ? this._path + '/' + uid : this._path) + '/view';
+  /* The factory we'll use to register with Angular */
+  Resource.$factory =  ['$http', '$q', function($http, $q) {
+    return function(path, options) {
+      return new Resource($http, $q, path, options);
     };
+  }];
 
-    Resource.prototype.find = function(uid) {
-        var deferred = this._q.defer();
+  /* Factory registration in Angular module */
+  angular.module('SOGo.Common').factory('sgResource', Resource.$factory);
 
-        this._http
-            .get(this.path(uid))
-            .success(deferred.resolve)
-            .error(deferred.reject);
+  /* Instance methods */
 
-        return deferred.promise;
-    };
+  Resource.prototype.path = function(uid) {
+    return (uid ? this._path + '/' + uid : this._path) + '/view';
+  };
 
-    Resource.prototype.filter = function(uid, params) {
-        var deferred = this._q.defer();
+  Resource.prototype.find = function(uid) {
+    var deferred = this._q.defer();
 
-        this._http({
-            method: 'GET',
-            url: this.path(uid),
-            params: params
-        })
-            .success(deferred.resolve)
-            .error(deferred.reject);
+    this._http
+      .get(this.path(uid))
+      .success(deferred.resolve)
+      .error(deferred.reject);
 
-        return deferred.promise;
-    };
+    return deferred.promise;
+  };
 
-    Resource.prototype.newguid = function(uid) {
-        var deferred = this._q.defer();
-        var path = this._path + '/' + uid + '/newguid';
+  Resource.prototype.filter = function(uid, params) {
+    var deferred = this._q.defer();
 
-        this._http
-            .get(path)
-            .success(deferred.resolve)
-            .error(deferred.reject);
+    this._http({
+      method: 'GET',
+      url: this.path(uid),
+      params: params
+    })
+      .success(deferred.resolve)
+      .error(deferred.reject);
 
-        return deferred.promise;
-    };
+    return deferred.promise;
+  };
 
-    /**
-     * @function create
-     * @desc Create a new resource using a specific action
-     * @param {string} action - the action to be used in the URL
-     * @param {string} name - the new resource's name
-     */
-    Resource.prototype.create = function(action, name) {
-        var deferred = this._q.defer();
-        var path = this._path + '/' + action;
+  Resource.prototype.newguid = function(uid) {
+    var deferred = this._q.defer(),
+        path = this._path + '/' + uid + '/newguid';
 
-        this._http
-            .post(path, { 'name': name })
-            .success(deferred.resolve)
-            .error(deferred.reject);
+    this._http
+      .get(path)
+      .success(deferred.resolve)
+      .error(deferred.reject);
 
-        return deferred.promise;
-    };
+    return deferred.promise;
+  };
 
-    Resource.prototype.save = function(uid, newValue, options) {
-        var deferred = this._q.defer();
-        var action = (options && options.action)? options.action : 'save';
-        var path = this._path + '/' + uid + '/' + action;
+  /**
+   * @function create
+   * @desc Create a new resource using a specific action
+   * @param {string} action - the action to be used in the URL
+   * @param {string} name - the new resource's name
+   */
+  Resource.prototype.create = function(action, name) {
+    var deferred = this._q.defer(),
+        path = this._path + '/' + action;
 
-        this._http
-            .post(path, newValue)
-            .success(deferred.resolve)
-            .error(deferred.reject);
+    this._http
+      .post(path, { name: name })
+      .success(deferred.resolve)
+      .error(deferred.reject);
 
-        return deferred.promise;
-    };
+    return deferred.promise;
+  };
 
-    Resource.prototype.remove = function(uid) {
-        var deferred = this._q.defer();
-        var path = this._path + '/' + uid + '/delete';
+  Resource.prototype.save = function(uid, newValue, options) {
+    var deferred = this._q.defer(),
+        action = (options && options.action)? options.action : 'save',
+        path = this._path + '/' + uid + '/' + action;
 
-        this._http
-            .get(path)
-            .success(deferred.resolve)
-            .error(deferred.reject);
+    this._http
+      .post(path, newValue)
+      .success(deferred.resolve)
+      .error(deferred.reject);
 
-        return deferred.promise;
-    };
+    return deferred.promise;
+  };
+
+  Resource.prototype.remove = function(uid) {
+    var deferred = this._q.defer(),
+        path = this._path + '/' + uid + '/delete';
+
+    this._http
+      .get(path)
+      .success(deferred.resolve)
+      .error(deferred.reject);
+
+    return deferred.promise;
+  };
 })();
--- UI/WebServerResources/js/SOGoRootPage.js
+++ UI/WebServerResources/js/SOGoRootPage.js
@@ -1,23 +1,23 @@
-/* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* JavaScript for SOGoRootPage */
 
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('SOGo.MainUI', ['SOGo.Authentication'])
+  angular.module('SOGo.MainUI', ['SOGo.Authentication'])
 
     .controller('loginController', ['$scope', 'Authentication', function($scope, Authentication) {
+      $scope.warning = false;
+      $scope.creds = { username: null, password: null };
+      $scope.login = function(creds) {
         $scope.warning = false;
-        $scope.creds = { 'username': null, 'password': null };
-        $scope.login = function(creds) {
-            $scope.warning = false;
-            Authentication.login(creds)
-                .then(function(url) {
-                    window.location.href = url;
-                }, function(msg) {
-                    $scope.warning = msg.error;
-                });
-            return false;
-        };
+        Authentication.login(creds)
+          .then(function(url) {
+            window.location.href = url;
+          }, function(msg) {
+            $scope.warning = msg.error;
+          });
+        return false;
+      };
     }]);
 })();
