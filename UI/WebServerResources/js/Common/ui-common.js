54444ddf3ba0fcd2134741a84053bb2bba38951e
Francis Lachapelle <flachapelle@inverse.ca>
2014-11-24 13:25:56 -0500

    AngularJS SOGo.UICommon module

---
+++ UI/WebServerResources/js/Common/ui-common.js
@@ -0,0 +1,22 @@
+/* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* JavaScript for common UI services for mobile theme */
+
+(function() {
+  'use strict';
+
+  /* Angular module instanciation */
+  angular.module('SOGo.UICommon', [])
+  
+    .filter('encodeUri', function ($window) {
+      return $window.encodeURIComponent;
+    })
+
+    .filter('decodeUri', function ($window) {
+      return $window.decodeURIComponent;
+    })
+
+    .filter('loc', function () {
+      return l;
+    });
+
+})();
