137e8e7290e61f5b45193f8cfc24eca585dd2c5c
Francis Lachapelle <flachapelle@inverse.ca>
2015-01-21 14:46:36 -0500

    Add [iCal(Repeatable)EntityObject attribtues]
    Those methods are used to get the common attributes of a iCalendar
    component (vEvent or vTodo).

--- SoObjects/Appointments/iCalRepeatableEntityObject+SOGo.h
+++ SoObjects/Appointments/iCalRepeatableEntityObject+SOGo.h
@@ -1,6 +1,6 @@
 /* iCalRepeatableEntityObject+SOGo.h - this file is part of SOGo
  *
- * Copyright (C) 2007-2014 Inverse inc.
+ * Copyright (C) 2007-2015 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
