255278f05cb45229d4832c7bbb79d87672074007
iRouge <bfavreault@inverse.ca>
2015-01-27 15:50:19 -0500

    Add import to bower_components ngMaterial

--- UI/WebServerResources/js/Common/ui.js
+++ UI/WebServerResources/js/Common/ui.js
@@ -13,25 +13,7 @@
   angular.module('SOGo.UI', ['ngMaterial'])
 
       .config(function ($mdThemingProvider) {
-// The default red paletette for warn-color
-        $mdThemingProvider.definePalette('red', {
-          '50': '#ffebee',
-          '100': '#ffcdd2',
-          '200': '#ef9a9a',
-          '300': '#e57373',
-          '400': '#ef5350',
-          '500': '#f44336',
-          '600': '#e53935',
-          '700': '#d32f2f',
-          '800': '#c62828',
-          '900': '#b71c1c',
-          'A100': '#ff8a80',
-          'A200': '#ff5252',
-          'A400': '#ff1744',
-          'A700': '#d50000',
-          'contrastDefaultColor': 'light',
-          'contrastDarkColors': '50 100 200 300 400 A100'
-        });
+
         $mdThemingProvider.definePalette('sogo-green', {
           '50': 'eaf5e9',
           '100': 'cbe5c8',
