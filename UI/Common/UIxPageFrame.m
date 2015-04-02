0fc027e5e922b5b5d59ae39c06f76ef5a9d108f6
Francis Lachapelle <flachapelle@inverse.ca>
2015-04-01 20:20:22 -0400

    Set the doctype to HTML5

--- UI/Common/UIxPageFrame.m
+++ UI/Common/UIxPageFrame.m
@@ -113,9 +113,7 @@
 
 - (NSString *) doctype
 {
-  return (@"<!DOCTYPE html"
-          @" PUBLIC \"-//W3C//DTD XHTML 1.1//EN\""
-          @" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">");
+  return (@"<!DOCTYPE html>");
 }
 
 /* Help URL/target */
