Alinto/sogo

commit ebe2a466e7ac3d6aa0f10befc9367555224ac752

oc-calendar: Compute PidLidAppointmentDefinitionStartWhole for all-day events

OpenChange/MAPIStoreAppointmentWrapper.m
@@ -2073,17 +2073,21 @@ ReservedBlockEE2Size: 00 00 00 00
   enum mapistore_error rc;
   iCalTimeZone *icalTZ;
 
-  if ([event isRecurrent])
+  /* [MS-OXOCAL] 3.1.5.5.1: This property is used in floating (all-day) events,
+     specified in floating time, to convert the start date from UTC to the user's
+     time zone */
+  if ([event isAllDay])
+    icalTZ = [iCalTimeZone timeZoneForName: [timeZone timeZoneName]];
+  else if ([event isRecurrent])
+    icalTZ = [(iCalDateTime *) [event firstChildWithTag: @"dtstart"] timeZone];
+
+  if (icalTZ)
     {
-      icalTZ = [(iCalDateTime *) [event firstChildWithTag: @"dtstart"] timeZone];
-      if (icalTZ)
-        {
-          *data = [icalTZ asZoneTimeDefinitionWithFlags: TZRULE_FLAG_EFFECTIVE_TZREG | TZRULE_FLAG_RECUR_CURRENT_TZREG
-                                               inMemCtx: memCtx];
-          rc = MAPISTORE_SUCCESS;
-        }
-      else
-        rc = MAPISTORE_ERR_NOT_FOUND;
+      /* [MS-OXOCAL] 2.2.1.42: This property can only have the E flag set in the
+         TimeZoneDefinition struct */
+      *data = [icalTZ asZoneTimeDefinitionWithFlags: TZRULE_FLAG_EFFECTIVE_TZREG
+                                           inMemCtx: memCtx];
+      rc = MAPISTORE_SUCCESS;
     }
   else
     rc = MAPISTORE_ERR_NOT_FOUND;

commit e2ef0103c4c05c38354b620a09fc83c8c299da0d

oc-calendar: Fix regression on AppointmentTimeZoneDefinitionStartDisplay
Introduced by ebe2a466e7 in PR #132 when the event is not
all day neither recurrent one.

The fix is just to initialise to nil when it is a normal event
and it returns NOT_FOUND for this property.

OpenChange/MAPIStoreAppointmentWrapper.m
@@ -2080,6 +2080,8 @@ ReservedBlockEE2Size: 00 00 00 00
     icalTZ = [iCalTimeZone timeZoneForName: [timeZone timeZoneName]];
   else if ([event isRecurrent])
     icalTZ = [(iCalDateTime *) [event firstChildWithTag: @"dtstart"] timeZone];
+  else
+    icalTZ = nil;
 
   if (icalTZ)
     {