7d018221ef753942711826b534f23f8e3bf7447e
Ludovic Marcotte <lmarcotte@inverse.ca>
2015-06-10 16:31:15 -0400

    Revert "Improve creation of CSS identifier"
    This reverts commit e40a19cf8990136e55ba728b4626ca1877e064be.

--- UI/WebServerResources/JavascriptAPIExtensions.js
+++ UI/WebServerResources/JavascriptAPIExtensions.js
@@ -95,12 +95,10 @@ String.prototype.asDate = function () {
 };
 
 String.prototype.asCSSIdentifier = function() {
-    var characters = [ '_'  ,  '\\.',  '#'  ,  '@'  ,  '\\*',  ':'  , ';'   , ','   , ' ',
-                       '\\(',    '\\)',    '\\[',    '\\]',    '\\{',    '\\}',
-                       , "'",  '"',    '&',    '\\+' ];
-    var escapeds =   [ '_U_',  '_D_',  '_H_',  '_A_',  '_S_',  '_C_', '_SC_', '_CO_', '_SP_',
-                       '_LP_', '_RP_', '_LS_', '_RQ_', '_LC_', '_RC_',
-                       '_SQ_', '_DQ_', '_AM_', '_P_' ];
+    var characters = [ '_'  , '\\.', '#'  , '@'  , '\\*', ':'  , ','   , ' '
+                       , "'", '&', '\\+' ];
+    var escapeds =   [ '_U_', '_D_', '_H_', '_A_', '_S_', '_C_', '_CO_',
+                       '_SP_', '_SQ_', '_AM_', '_P_' ];
 
     var newString = this;
     for (var i = 0; i < characters.length; i++) {
