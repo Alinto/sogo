33d0f30d97a7f038d2eb90553455ba79fb35cb75
Ludovic Marcotte <lmarcotte@inverse.ca>
2015-08-06 16:17:31 -0400

    (fix) avoid closing the email composition window by error

--- UI/WebServerResources/js/Mailer/MailboxController.js
+++ UI/WebServerResources/js/Mailer/MailboxController.js
@@ -100,8 +100,8 @@
       $mdDialog.show({
         parent: angular.element(document.body),
         targetEvent: $event,
-        clickOutsideToClose: true,
-        escapeToClose: true,
+        clickOutsideToClose: false,
+        escapeToClose: false,
         templateUrl: 'UIxMailEditor',
         controller: 'MessageEditorController',
         controllerAs: 'editor',
