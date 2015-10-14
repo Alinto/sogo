a54cc22da8c690c7ec4b9c03b99349be7b326262
Ludovic Marcotte <lmarcotte@inverse.ca>
2015-10-14 09:29:52 -0400

    (fix) we no longer always rewrite emails for Outlook when using EAS

--- ActiveSync/SOGoMailObject+ActiveSync.m
+++ ActiveSync/SOGoMailObject+ActiveSync.m
@@ -541,7 +541,7 @@ struct GlobalObjectId {
       // We sanitize the content *ONLY* for Outlook clients and if the content-transfer-encoding is 8bit. Outlook has strange issues
       // with quoted-printable/base64 encoded text parts. It just doesn't decode them.
       encoding = [[self lookupInfoForBodyPart: @""] objectForKey: @"encoding"];
-      if ([[context objectForKey: @"DeviceType"] isEqualToString: @"WindowsOutlook15"] || ([encoding caseInsensitiveCompare: @"8bit"] == NSOrderedSame))
+      if (encoding && ([encoding caseInsensitiveCompare: @"8bit"] == NSOrderedSame))
         d = [self _sanitizedMIMEMessage];
       else
         d = [self content];
