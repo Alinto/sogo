4d4522039a73c4f649d5e8023553b4f20ccc000a
Euan Thoms <euan@potensol.com>
2015-11-04 05:54:12 +0800

    Stage 7 of clang compiler warning patches.

--- SoObjects/Mailer/SOGoMailAccounts.m
+++ SoObjects/Mailer/SOGoMailAccounts.m
@@ -41,6 +41,8 @@
 
 #define XMLNS_INVERSEDAV @"urn:inverse:params:xml:ns:inverse-dav"
 
+// TODO: prune redundant methods
+
 @implementation SOGoMailAccounts
 
 - (NSArray *) mailAccounts
@@ -206,6 +208,9 @@
 </D:multistatus>
 
 */
+
+/* No longer in use, causes objc-method-access warning
+
 - (NSException *) setDavMailsLabels: (NSString *) newLabels
 {
   id <DOMElement> documentElement, labelNode;
@@ -253,5 +258,6 @@
 
   return nil;
 }
+*/
 
 @end /* SOGoMailAccounts */
