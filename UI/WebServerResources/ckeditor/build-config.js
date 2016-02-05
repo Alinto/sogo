a9083004ef007e404fff1dad55a54d55e0a514cf
Francis Lachapelle <flachapelle@inverse.ca>
2016-02-05 12:11:36 -0500

    Update CKEditor to version 4.5.7

--- UI/WebServerResources/ckeditor/build-config.js
+++ UI/WebServerResources/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/2fa8e29f45e897c52b5acc6c12fe29b2
+ * (2) http://ckeditor.com/builder/bbef0ab675ca380257ce73ba0657ee80
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/2fa8e29f45e897c52b5acc6c12fe29b2
+ * (3) http://ckeditor.com/builder/download/bbef0ab675ca380257ce73ba0657ee80
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -103,6 +103,7 @@ var CKBUILDER_CONFIG = {
 		'eu' : 1,
 		'fi' : 1,
 		'fr' : 1,
+		'hr' : 1,
 		'hu' : 1,
 		'is' : 1,
 		'it' : 1,
