e5fe211dcdd99b9c9f25c5d95536e7dd64a9d101
Francis Lachapelle <flachapelle@inverse.ca>
2016-02-25 08:59:02 -0500

    (js) Add missing dependency to msg popup view
    Fixes #3545

--- UI/WebServerResources/js/Mailer/Mailer.popup.js
+++ UI/WebServerResources/js/Mailer/Mailer.popup.js
@@ -4,7 +4,7 @@
 (function() {
   'use strict';
 
-  angular.module('SOGo.MailerUI', ['ui.router', 'ck', 'angularFileUpload', 'SOGo.Common', 'SOGo.ContactsUI', 'ngAnimate', 'SOGo.PreferencesUI'])
+  angular.module('SOGo.MailerUI', ['ui.router', 'ck', 'angularFileUpload', 'SOGo.Common', 'SOGo.ContactsUI', 'SOGo.SchedulerUI', 'ngAnimate', 'SOGo.PreferencesUI'])
     .config(configure)
     .run(runBlock)
     .controller('MessageEditorControllerPopup', MessageEditorControllerPopup);
