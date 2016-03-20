b9ef3db5e9107402c23a288cbedb1aa2754b85a2
InverseBot <sogo@inverse.ca>
2016-03-20 01:35:49 -0400

    (js/css) Update generated files

--- UI/WebServerResources/js/vendor/angular-material.js
+++ UI/WebServerResources/js/vendor/angular-material.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.0.6-master-0855fd3
+ * v1.0.6-master-991cd7a
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -25571,4 +25571,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "/*  Only used with Th
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.0.6-master-0855fd3"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.0.6-master-991cd7a"}};
