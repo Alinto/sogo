11844283d1bc8bf82c5c6516f2ebdb91dc67dbeb
Ludovic Marcotte <lmarcotte@inverse.ca>
2016-05-10 15:51:32 -0400

    (fix) don't use removeItemAtPath - it is not available on Precise
    Conflicts:
    
    	SoObjects/Mailer/SOGoDraftObject.m

--- SoObjects/Mailer/SOGoDraftObject.m
+++ SoObjects/Mailer/SOGoDraftObject.m
@@ -1160,7 +1160,7 @@ static NSString    *userAgent      = nil;
       pmime = [self pathToAttachmentWithName: [NSString stringWithFormat: @".%@.mime", name]];
       if (![[mimeType dataUsingEncoding: NSUTF8StringEncoding] writeToFile: pmime atomically: YES])
         {
-          [[NSFileManager defaultManager] removeItemAtPath: p error: nil];
+          [[NSFileManager defaultManager] removeFileAtPath: p  handler: nil];
           return [NSException exceptionWithHTTPStatus: 500 /* Server Error */
                                                reason: @"Could not write attachment to draft!"];
         }
