b5b9d596f0638c89d3815a594cf6c79d13a7c422
Ludovic Marcotte <lmarcotte@inverse.ca>
2016-05-11 10:16:34 -0400

    (fix) handle multi-value org units

--- SoObjects/Contacts/NGVCard+SOGo.m
+++ SoObjects/Contacts/NGVCard+SOGo.m
@@ -293,7 +293,9 @@ convention:
                   atIndex: 6 forKey: @""];
 
   ou = [ldifRecord objectForKey: @"ou"];
-  if (ou)
+  if ([ou isKindOfClass: [NSArray class]])
+    units = [NSArray arrayWithArray: ou];
+  else if (ou)
     units = [NSArray arrayWithObject: ou];
   else
     units = nil;
