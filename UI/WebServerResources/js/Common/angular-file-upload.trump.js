6ba6dcbf096e8c4ba81a18e73cf2b80b1c4744af
Francis Lachapelle <flachapelle@inverse.ca>
2016-07-04 12:31:57 -0400

    (js) Fix mail editor when leaving dropping area

--- UI/WebServerResources/js/Common/angular-file-upload.trump.js
+++ UI/WebServerResources/js/Common/angular-file-upload.trump.js
@@ -3,16 +3,16 @@
 (function() {
   'use strict';
 
+  angular
+    .module('angularFileUpload')
+    .decorator('FileUploader', FileUploaderDecorator)
+    .decorator('FileDrop', FileDropDecorator);
+
   /**
    * Set XSRF header if the cookie exists.
    * Solution based on the following discussion:
    * https://github.com/nervgh/angular-file-upload/issues/360
-   */
-  angular
-    .module('angularFileUpload')
-    .decorator('FileUploader', FileUploaderDecorator);
-
-  /**
+   *
    * @ngInject
    */
   FileUploaderDecorator.$inject = ['$delegate', '$cookies'];
@@ -21,6 +21,34 @@
       var token = $cookies.get('XSRF-TOKEN');
       if (token)
         item.headers = {'X-XSRF-TOKEN': token};
+    };
+    return $delegate;
+  }
+
+  /**
+   * Fixed bug causing nv-file-over (over-class) not resetting when leaving element.
+   * Solution based on this unmerged pull request:
+   * https://github.com/nervgh/angular-file-upload/pull/643
+   *
+   * @ngInject
+   */
+  FileDropDecorator.$inject = ['$delegate', '$timeout'];
+  function FileDropDecorator($delegate, $timeout) {
+    $delegate.prototype.onDragOver = function(event) {
+      var transfer = this._getTransfer(event);
+      if (!this._haveFiles(transfer.types)) return;
+      transfer.dropEffect = 'copy';
+      this._preventAndStop(event);
+      angular.forEach(this.uploader._directives.over, this._addOverClass, this);
+      $timeout.cancel(this.onDragLeaveTimer);
+    };
+    $delegate.prototype.onDragLeave = function(event) {
+      var that = this;
+      $timeout.cancel(this.onDragLeaveTimer);
+      this.onDragLeaveTimer = $timeout(function() {
+        that._preventAndStop(event);
+        angular.forEach(that.uploader._directives.over, that._removeOverClass, that);
+      }, 50);
     };
     return $delegate;
   }
