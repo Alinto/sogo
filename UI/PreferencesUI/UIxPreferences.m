be12528a0ee8e8c3bb23ac37247e76482b060fbc
Francis Lachapelle <flachapelle@inverse.ca>
2016-10-04 06:14:41 -0400

    Fix condition in UIxPreferences.m

--- UI/PreferencesUI/UIxPreferences.m
+++ UI/PreferencesUI/UIxPreferences.m
@@ -1671,8 +1671,8 @@ static NSArray *reminderValues = nil;
                     stringByTrimmingSpaces];
 
           /* We make sure that the "custom" value is different from the system email */
-          if ([value length] == 0)
-            || [[user systemEmail] isEqualToString: value])
+          if ([value length] == 0
+              || [[user systemEmail] isEqualToString: value])
             value = nil;
           [userDefaults setMailCustomEmail: value];
 
