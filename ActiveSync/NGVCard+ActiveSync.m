3484cd570c6aed0f3d531b0c3e317514994abc38
Ludovic Marcotte <lmarcotte@inverse.ca>
2016-12-20 08:56:13 -0500

    (fix) avoid double encoding (fixes #3960)

--- ActiveSync/NGVCard+ActiveSync.m
+++ ActiveSync/NGVCard+ActiveSync.m
@@ -190,7 +190,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
       if ((o = [homeAdr flattenedValueAtIndex: 1  forKey: @""]) && [o length])
         [a appendFormat: @"\n%@", [o activeSyncRepresentationInContext: context]];
       
-      [s appendFormat: @"<HomeStreet xmlns=\"Contacts:\">%@</HomeStreet>", [a activeSyncRepresentationInContext: context]];
+      [s appendFormat: @"<HomeStreet xmlns=\"Contacts:\">%@</HomeStreet>", a];
 
       if ((o = [homeAdr flattenedValueAtIndex: 3  forKey: @""]))
         [s appendFormat: @"<HomeCity xmlns=\"Contacts:\">%@</HomeCity>", [o activeSyncRepresentationInContext: context]];
@@ -221,7 +221,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
       if ((o = [workAdr flattenedValueAtIndex: 1  forKey: @""]) && [o length])
         [a appendFormat: @"\n%@", [o activeSyncRepresentationInContext: context]];
 
-      [s appendFormat: @"<BusinessStreet xmlns=\"Contacts:\">%@</BusinessStreet>", [a activeSyncRepresentationInContext: context]];
+      [s appendFormat: @"<BusinessStreet xmlns=\"Contacts:\">%@</BusinessStreet>", a];
       
       if ((o = [workAdr flattenedValueAtIndex: 3  forKey: @""]))
         [s appendFormat: @"<BusinessCity xmlns=\"Contacts:\">%@</BusinessCity>", [o activeSyncRepresentationInContext: context]];
