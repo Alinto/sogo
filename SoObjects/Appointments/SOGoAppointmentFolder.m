457fccd5ac9f000f1125ba91266a268b626573c3
Ludovic Marcotte <lmarcotte@inverse.ca>
2016-12-21 11:43:08 -0500

    (fix) append .ics extension if not present when importing events (fixes #2308)

--- SoObjects/Appointments/SOGoAppointmentFolder.m
+++ SoObjects/Appointments/SOGoAppointmentFolder.m
@@ -3237,6 +3237,12 @@ firstInstanceCalendarDateRange: (NGCalendarDateRange *) fir
         }
     }
 
+  // If the UID isn't ending with the ".ics" extension, let's add it to avoid
+  // confusing broken CalDAV client (like Nokia N9 and Korganizer) that relies
+  // on this (see #2308)
+  if (![[uid lowercaseString] hasSuffix: @".ics"])
+    uid = [NSString stringWithFormat: @"%@.ics", uid];
+
   object = [SOGoAppointmentObject objectWithName: uid
                                     inContainer: self];
   [object setIsNew: YES];
@@ -3400,8 +3406,7 @@ firstInstanceCalendarDateRange: (NGCalendarDateRange *) fir
                                   timezone: timezone]))
             {
               imported++;
-              [uids setValue: uid
-                      forKey: originalUid];
+              [uids setValue: uid  forKey: originalUid];
             }
         }
       
