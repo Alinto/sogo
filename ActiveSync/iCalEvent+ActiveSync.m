926cf6f34cabf9fc22488596133943e5612a1a64
Ludovic Marcotte <lmarcotte@inverse.ca>
2017-01-06 09:54:02 -0500

    (fix) make sure to bump LAST-MODIFIED to trigger all auto-scheduling code

--- ActiveSync/iCalEvent+ActiveSync.m
+++ ActiveSync/iCalEvent+ActiveSync.m
@@ -891,6 +891,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
           [self setAttendees: attendees];
         }
     }
+
+  [self setLastModified: [NSCalendarDate calendarDate]];
 }
 
 - (void) changeParticipationStatus: (NSDictionary *) theValues
