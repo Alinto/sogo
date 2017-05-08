0e82550f6b49d31b5fba2e0cb14e77bd166780f5
Francis Lachapelle <flachapelle@inverse.ca>
2017-05-08 14:44:13 -0400

    Update SOGoParentFolder.h

--- SoObjects/SOGo/SOGoParentFolder.h
+++ SoObjects/SOGo/SOGoParentFolder.h
@@ -1,6 +1,6 @@
 /* SOGoParentFolder.h - this file is part of SOGo
  *
- * Copyright (C) 2006-2015 Inverse inc.
+ * Copyright (C) 2006-2017 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,6 +44,7 @@
 - (NSString *) collectedFolderName;
 
 - (NSException *) appendPersonalSources;
+- (NSException *) appendSubscribedSources;
 - (void) removeSubFolder: (NSString *) subfolderName;
 
 - (void) setBaseOCSPath: (NSString *) newOCSPath;
