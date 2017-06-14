b6b8f7a0fb2e8265ac4d66107e4c95851d65a41a
Francis Lachapelle <flachapelle@inverse.ca>
2017-06-14 17:03:31 -0400

    Improve display of component comments, card notes

---
+++ UI/WebServerResources/js/Common/txt2html.filter.js
@@ -0,0 +1,23 @@
+/* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+/**
+ * txt2html - A filter to convert line feeds and carriage returns to html line breaks
+ * @memberof SOGo.Common
+ */
+(function () {
+  'use strict';
+
+  /**
+   * @ngInject
+   */
+  txt2html.$inject = ['linkyFilter'];
+  function txt2html(linkyFilter) {
+    return function(text) {
+      // Linky will first sanitize the text; linefeeds are therefore encoded.
+      return text ? String(linkyFilter(text, ' _blank')).replace(/&#10;/gm, '<br>') : undefined;
+    };
+  }
+
+  angular.module('SOGo.Common')
+    .filter('txt2html', txt2html);
+})();
