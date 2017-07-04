6272cb5a8554f27133948ec0e00b0a83d9592532
Francis Lachapelle <flachapelle@inverse.ca>
2017-07-04 17:22:01 -0400

    (js) Improve regular expression for email address

--- UI/WebServerResources/generic.js
+++ UI/WebServerResources/generic.js
@@ -117,7 +117,7 @@ function extractEmailAddress(mailTo) {
     var email = "";
 
     var emailre
-        = /([a-zA-Z0-9\._\-]*[a-zA-Z0-9_\-]+@[a-zA-Z0-9\._\-]*[a-zA-Z0-9])/;
+        = /([a-zA-Z0-9\._\-]*[a-zA-Z0-9_\-%\+]+@[a-zA-Z0-9\._\-]*[a-zA-Z0-9])/;
     if (emailre.test(mailTo)) {
         emailre.exec(mailTo);
         email = RegExp.$1;
