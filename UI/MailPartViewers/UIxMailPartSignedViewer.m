22c078ee76bbc729095686eae07250748194df35
Ludovic Marcotte <lmarcotte@inverse.ca>
2017-07-04 11:00:06 -0400

    (fix) enable S/MIME even when using GNU TLS (fixes #4201)

--- UI/MailPartViewers/UIxMailPartSignedViewer.m
+++ UI/MailPartViewers/UIxMailPartSignedViewer.m
@@ -18,7 +18,8 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#ifdef HAVE_OPENSSL
+#if defined(HAVE_OPENSSL) || defined(HAVE_GNUTLS)
+
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/pkcs7.h>
@@ -35,7 +36,8 @@
 
 @implementation UIxMailPartSignedViewer : UIxMailPartMixedViewer
 
-#ifdef HAVE_OPENSSL
+
+#if defined(HAVE_OPENSSL) || defined(HAVE_GNUTLS)
 - (BOOL) supportsSMIME
 {
   return YES;
