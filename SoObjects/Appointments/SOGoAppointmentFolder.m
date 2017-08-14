e1791ac8f54f61ca4afe6b2b04f947b841795815
Ludovic Marcotte <lmarcotte@inverse.ca>
2017-08-14 13:59:16 -0400

    (fix) offset can be negative (fixes #4237)

--- SoObjects/Appointments/SOGoAppointmentFolder.m
+++ SoObjects/Appointments/SOGoAppointmentFolder.m
@@ -1241,7 +1241,8 @@ firstInstanceCalendarDateRange: (NGCalendarDateRange *) fir
   iCalDateTime *dtstart;
   iCalRepeatableEntityObject *component;
   iCalTimeZone *eventTimeZone;
-  unsigned count, max, offset;
+  unsigned count, max;
+  int offset;
   id tz;
 
   content = [theRecord objectForKey: @"c_cycleinfo"];
