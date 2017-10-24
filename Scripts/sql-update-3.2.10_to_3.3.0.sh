f5aeed444d80a5510fa146b964fca354c8aa33e6
Francis Lachapelle <flachapelle@inverse.ca>
2017-10-24 14:19:25 -0400

    Increase mail column size of Contacts quick tables
    Fixes #4322

--- Scripts/sql-update-3.2.10_to_3.3.0.sh
+++ Scripts/sql-update-3.2.10_to_3.3.0.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 set -e
-
-# This script only works with MySQL
-# updates c_defaults and c_settings to longtext in the sogo_user_profile table
-# to avoid truncation of data at 64k
-
+# This script only works with PostgreSQL
+# updates c_mail to text in Contacts quick table
+# http://www.sogo.nu/bugs/view.php?id=4322
 
 defaultusername=$USER
-defaulthostname=127.0.0.1
+defaulthostname=localhost
 defaultdatabase=sogo
 indextable=$(sogo-tool dump-defaults -f /etc/sogo/sogo.conf | awk -F\" '/ OCSFolderInfoURL =/  {print $2}' |  awk -F/ '{print $NF}')
 if [ -z "$indextable" ]; then
@@ -24,12 +22,10 @@ if [ -z "$username" ]
 then
   username=$defaultusername
 fi
-
 if [ -z "$hostname" ]
 then
   hostname=$defaulthostname
 fi
-
 if [ -z "$database" ]
 then
   database=$defaultdatabase
@@ -37,33 +33,21 @@ fi
 
 sqlscript=""
 
-function growUserProfile() {
-    oldIFS="$IFS"
-    IFS=" "
-    part="`echo -e \"ALTER TABLE sogo_user_profile MODIFY c_defaults LONGTEXT;\\n\"`";
-    sqlscript="$sqlscript$part"
-    part="`echo -e \"ALTER TABLE sogo_user_profile MODIFY c_settings LONGTEXT;\\n\"`";
-    sqlscript="$sqlscript$part"
-    IFS="$oldIFS"
-}
-
 function growContactsQuick() {
     oldIFS="$IFS"
     IFS=" "
-    part="`echo -e \"ALTER TABLE $table MODIFY c_mail text;\\n\"`";
+    part="`echo -e \"ALTER TABLE $table ALTER COLUMN c_mail TYPE TEXT;\\n\"`";
     sqlscript="$sqlscript$part"
     IFS="$oldIFS"
 }
 
 echo "This script will ask for the database password twice" >&2
-echo "Converting c_content from TEXT to LONGTEXT in the sogo_user_profile table" >&2
-growUserProfile
-
 echo "Converting c_mail from VARCHAR(255) to TEXT in Contacts quick tables" >&2
-tables=`mysql -p -s -u $username -h $hostname $database -e "select SUBSTRING_INDEX(c_quick_location, '/', -1) from $indextable where c_path3 = 'Contacts';"`
+tables=`psql -t -U $username -h $hostname $database -c "select split_part(c_quick_location, '/', 5) from $indextable where c_path3 = 'Contacts';"`
+
 for table in $tables;
 do
   growContactsQuick
 done
 
-echo "$sqlscript" | mysql -p -s -u $username -h $hostname $database
+echo "$sqlscript" | psql -q -e -U $username -h $hostname $database
