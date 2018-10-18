Alinto/sogo

commit c8f7df402c96b785f23cbed42e0c84aeafa1daef

Don't use SSL_load_error_strings() and friends for OpenSSL-1.1.0.

UI/MailPartViewers/UIxMailPartSignedViewer.m
@@ -169,11 +169,20 @@
 
       if (err)
         {
-          ERR_load_crypto_strings();
+#ifdef HAVE_GNUTLS
+	  ERR_load_crypto_strings();
           SSL_load_error_strings();
           sslError = ERR_reason_error_string(err);
           validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : "(no error information available)"]] retain];
-        }
+#elseif OPENSSL_VERSION_NUMBER < 0x10100000L
+	  ERR_load_crypto_strings();
+          SSL_load_error_strings();
+          sslError = ERR_reason_error_string(err);
+          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError]] retain];
+#else
+	  validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError]] retain];
+#endif /* HAVE_GNUTLS */
+      }
     }
 
   

commit c4c7cf1742a02017c0f2947ad555a3f5a5d434d2

Fix typo #elseif introduced in c8f7df402c96b785f23cbed42e0c84aeafa1daef.

UI/MailPartViewers/UIxMailPartSignedViewer.m
@@ -174,7 +174,7 @@
           SSL_load_error_strings();
           sslError = ERR_reason_error_string(err);
           validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"No error information available"]] retain];
-#elseif OPENSSL_VERSION_NUMBER < 0x10100000L
+#elif OPENSSL_VERSION_NUMBER < 0x10100000L
           const char* sslError;
 	  ERR_load_crypto_strings();
           SSL_load_error_strings();