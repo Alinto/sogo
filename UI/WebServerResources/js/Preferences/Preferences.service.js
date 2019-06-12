868057698c51de871b6bf76ada566972afd9d054
Francis Lachapelle <flachapelle@inverse.ca>
2019-06-12 16:37:43 -0400

    (js) Allow 2-letter short month names
    Fixes #4766

--- UI/WebServerResources/js/Preferences/Preferences.service.js
+++ UI/WebServerResources/js/Preferences/Preferences.service.js
@@ -158,7 +158,7 @@
         dateString = dateString.trim();
         // The default function of Angular Material doesn't handle non-latin characters.
         // This one does.
-        var re = /^((([a-zA-Z]|[^\x00-\x7F]){3,}|[0-9]{1,4})([ .,]+|[/-])){2}(([a-zA-Z]|[^\x00-\x7F]){3,}|[0-9]{1,4})$/;
+        var re = /^((([a-zA-Z]|[^\x00-\x7F]){2,}|[0-9]{1,4})([ .,]+|[/-])){2}(([a-zA-Z]|[^\x00-\x7F]){3,}|[0-9]{1,4})$/;
         return re.test(dateString);
       };
     }
