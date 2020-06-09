9534816a2191106d36063a580d7bdfdd3ed03761
Johannes Kanefendt <johannes.kanefendt@krzn.de>
2020-06-09 13:55:08 +0200

    Fixed some header comments

--- SoObjects/SOGo/SOGoZipArchiver.h
+++ SoObjects/SOGo/SOGoZipArchiver.h
@@ -1,6 +1,6 @@
-/* SOGoCredentialsFile.h - this file is part of SOGo
+/* SOGoZipArchiver.h - this file is part of SOGo
  *
- * Copyright (C) 2013 Inverse inc.
+ * Copyright (C) 2020 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- SoObjects/SOGo/SOGoZipArchiver.m
+++ SoObjects/SOGo/SOGoZipArchiver.m
@@ -1,6 +1,6 @@
-/* SOGoCredentialsFile.m - this file is part of SOGo
+/* SOGoZipArchiver.m - this file is part of SOGo
  *
- * Copyright (C) 2013 Inverse inc.
+ * Copyright (C) 2020 Inverse inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
