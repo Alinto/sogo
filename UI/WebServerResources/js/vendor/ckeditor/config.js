3f308e0689422612ecd7b5bba4a636af6cd0f91e
Francis Lachapelle <flachapelle@inverse.ca>
2020-06-19 11:51:13 -0400

    chore(npm): update CKEditor to version 4.14.1
    Move from the minimalist skin to NT1 because the minimalist skin was
    buggy.

--- UI/WebServerResources/js/vendor/ckeditor/config.js
+++ UI/WebServerResources/js/vendor/ckeditor/config.js
@@ -1,54 +1,34 @@
 /**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-  // Define changes to default configuration here.
-  // For the complete reference see:
-  // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-  // The toolbar groups arrangement, optimized for a single toolbar row.
-  config.toolbarGroups = [
-    { name: 'basicstyles', groups: [ 'basicstyles' ] },
-    { name: 'colors' },
-    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align' ] },
-    { name: 'links' },
-    { name: 'insert' },
-    { name: 'editing', groups: [ 'spellchecker' ] },
-    { name: 'styles' },
-    { name: 'mode' }
-  ];
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+	{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+	{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+	{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+	{ name: 'forms' },
+	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+	{ name: 'links' },
+	{ name: 'insert' },
+	{ name: 'styles' },
+	{ name: 'colors' },
+	{ name: 'tools' },
+	{ name: 'others' },
+	{ name: 'about' }
+	];
 
-  // The default plugins included in the basic setup define some buttons that
-  // are not needed in a basic editor. They are removed here.
-  config.removeButtons = 'Strike,Subscript,Superscript,BGColor,Anchor,Format,Image';
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
 
-  // Dialog windows are also simplified.
-  config.removeDialogTabs = 'link:advanced';
-
-  config.enterMode = CKEDITOR.ENTER_BR;
-  config.tabSpaces = 4;
-  // config.fullPage = true; include header and body
-  config.allowedContent = true; // don't filter tags
-  config.entities = false;
-
-  // Configure autogrow
-  // https://ckeditor.com/docs/ckeditor4/latest/guide/dev_autogrow.html
-  config.autoGrow_onStartup = true;
-  config.autoGrow_minHeight = 300;
-  config.autoGrow_bottomSpace = 0;
-
-  // Disables the built-in words spell checker if browser provides one. Defaults to true.
-  // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html#cfg-disableNativeSpellChecker
-  //config.disableNativeSpellChecker = false;
-
-  // Whether to show the browser native context menu when the Ctrl or Meta (Mac) key is pressed on opening the context
-  // menu with the right mouse button click or the Menu key. Defaults to true.
-  // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html#cfg-browserContextMenuOnCtrl
-  //config.browserContextMenuOnCtrl = false;
-
-  // If enabled, turns on SCAYT automatically after loading the editor. Defaults to false.
-  // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html#cfg-scayt_autoStartup
-  //config.scayt_autoStartup = true;
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
 };
