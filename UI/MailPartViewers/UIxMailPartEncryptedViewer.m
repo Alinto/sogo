e2e5e6fed9b5d44c5d02374ab73449fab6de5e47
Francis Lachapelle <flachapelle@inverse.ca>
2020-11-26 16:37:13 -0500

    i18n(mail): improve generic error message for signed/encrypted messages
    Fixes #5204

--- UI/MailPartViewers/UIxMailPartEncryptedViewer.m
+++ UI/MailPartViewers/UIxMailPartEncryptedViewer.m
@@ -175,15 +175,15 @@
 	  ERR_load_crypto_strings();
           SSL_load_error_strings();
           sslError = ERR_reason_error_string(err);
-          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"No error information available"]] retain];
+          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"Digital signature is not valid"]] retain];
 #elif OPENSSL_VERSION_NUMBER < 0x10100000L
           const char* sslError;
 	  ERR_load_crypto_strings();
           SSL_load_error_strings();
           sslError = ERR_reason_error_string(err);
-          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"No error information available"]] retain];
+          validationMessage = [[self labelForKey: [NSString stringWithUTF8String: sslError ? sslError : @"Digital signature is not valid"]] retain];
 #else
-	  validationMessage = [[self labelForKey: @"No error information available"] retain];
+	  validationMessage = [[self labelForKey: @"Digital signature is not valid"] retain];
 #endif /* HAVE_GNUTLS */
 
            BUF_MEM *bptr; //DEL
