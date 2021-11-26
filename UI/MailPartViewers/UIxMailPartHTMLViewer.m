d079cafad2828b0adf96b4e33917f05b276239f3
Filip Paczyński <filip.paczynski@promonotes.pl>
2021-11-26 15:25:38 +0100

    fix(mail(viewer)): don't make all URLs lowercase
    Let's not make all urls lowercase, while still using lowercase version to detect "javascript:" and such.

--- UI/MailPartViewers/UIxMailPartHTMLViewer.m
+++ UI/MailPartViewers/UIxMailPartHTMLViewer.m
@@ -446,7 +446,7 @@ _xmlCharsetForCharset (NSString *charset)
            attributes: (id <SaxAttributes>) _attributes
 {
   unsigned int count, max;
-  NSString *name, *value, *cid, *lowerName;
+  NSString *name, *value, *cid, *lowerName, *lowerValue;
   NSMutableString *resultPart;
   BOOL skipAttribute;
 
@@ -518,11 +518,12 @@ _xmlCharsetForCharset (NSString *charset)
                        || [name isEqualToString: @"action"]
                        || [name isEqualToString: @"formaction"])
                 {
-                  value = [[_attributes valueAtIndex: count] lowercaseString];
-                  skipAttribute = ([value rangeOfString: @"://"].location == NSNotFound
-                                   && ![value hasPrefix: @"mailto:"]
-                                   && ![value hasPrefix: @"#"]) ||
-                    [value hasPrefix: @"javascript:"];
+                  value = [_attributes valueAtIndex: count];
+                  lowerValue = [value lowercaseString];
+                  skipAttribute = ([lowerValue rangeOfString: @"://"].location == NSNotFound
+                                   && ![lowerValue hasPrefix: @"mailto:"]
+                                   && ![lowerValue hasPrefix: @"#"]) ||
+                    [lowerValue hasPrefix: @"javascript:"];
                   if (!skipAttribute)
                     [resultPart appendString: @" rel=\"noopener\""];
                 }
