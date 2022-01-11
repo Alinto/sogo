487e67a291d757cf74079a68d137472c009c3305
Francis Lachapelle <flachapelle@inverse.ca>
2022-01-11 13:41:49 -0500

    fix(eas): handle module constraints
    Fixes #5456

--- ActiveSync/SOGoActiveSyncDispatcher.m
+++ ActiveSync/SOGoActiveSyncDispatcher.m
@@ -676,6 +676,12 @@ void handle_eas_terminate(int signum)
         SOGoMailFolder *folderToUpdate;
 
         accountsFolder = [userFolder lookupName: @"Mail"  inContext: context  acquire: NO];
+        if ([accountsFolder isKindOfClass: [NSException class]])
+          {
+            [theResponse setStatus: 403];
+            [self logWithFormat: @"Mail - Forbidden access for user %@", [[context activeUser] loginInDomain]];
+            return;
+          }
         currentFolder = [accountsFolder lookupName: @"0"  inContext: context  acquire: NO];
   
         folderToUpdate = [currentFolder lookupName: [NSString stringWithFormat: @"folder%@", serverId]
@@ -721,6 +727,12 @@ void handle_eas_terminate(int signum)
 	NSString *nameInCache;
 
         appointmentFolders = [userFolder privateCalendars: @"Calendar" inContext: context];
+        if ([appointmentFolders isKindOfClass: [NSException class]])
+          {
+            [theResponse setStatus: 403];
+            [self logWithFormat: @"Calendar - Forbidden access for user %@", [[context activeUser] loginInDomain]];
+            return;
+          }
 
         folderToUpdate = [appointmentFolders lookupName: [NSString stringWithFormat: @"%@", serverId]
                                               inContext: context
