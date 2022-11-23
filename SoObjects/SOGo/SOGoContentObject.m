Alinto/sogo

commit c62b043e26111ee4d6073b9b37931e15f3b95753

fix(addressbook): Fix NSException on address book where uppercaseString is called on data

SoObjects/SOGo/SOGoContentObject.m
@@ -97,8 +97,10 @@
   int intValue;
 
   data = [objectRecord objectForKey: @"c_content"];
-  if (data)
+  if (data && [data isKindOfClass: [NSString class]])
     ASSIGN (content, data);
+  else
+    ASSIGN (content, [NSString stringWithUTF8String:[data bytes]]);
   data = [objectRecord objectForKey: @"c_version"];
   if (data)
     version = [data unsignedIntValue];

SoObjects/Contacts/SOGoContactGCSEntry.m
@@ -62,7 +62,7 @@
 {
   if (!card)
     {
-      if ([[content uppercaseString] hasPrefix: @"BEGIN:VCARD"])
+      if ([[[self contentAsString] uppercaseString] hasPrefix: @"BEGIN:VCARD"])
         card = [NGVCard parseSingleFromSource: content];
       else
         card = [NGVCard cardWithUid: [self nameInContainer]];

commit d44e1d7b45471f847508a091e78bd6d234c8a4bb

Fix issue #0005644.
Fixes a NULL exception introduced by c62b043e26111ee4d6073b9b37931e15f3b95753.

SoObjects/SOGo/SOGoContentObject.m
@@ -97,10 +97,13 @@
   int intValue;
 
   data = [objectRecord objectForKey: @"c_content"];
-  if (data && [data isKindOfClass: [NSString class]])
-    ASSIGN (content, data);
-  else
-    ASSIGN (content, [NSString stringWithUTF8String:[data bytes]]);
+  if (data)
+    {
+      if ([data isKindOfClass: [NSString class]])
+        ASSIGN (content, data);
+      else
+        ASSIGN (content, [NSString stringWithUTF8String:[data bytes]]);
+    }
   data = [objectRecord objectForKey: @"c_version"];
   if (data)
     version = [data unsignedIntValue];