fe9ae12e46a151ee5989ed1f0009bb81611a46bd
smizrahi <seb.mizrahi@gmail.com>
2022-12-05 11:45:45 +0100

    fix(security): Security fix for WSTG-INPV-02. Fix NSException where tried to modify NSDictionary. Closes #5651.

--- SoObjects/SOGo/SOGoUserDefaults.m
+++ SoObjects/SOGo/SOGoUserDefaults.m
@@ -805,9 +805,9 @@ NSString *SOGoPasswordRecoverySecondaryEmail = @"SecondaryEmail";
   // Remove possible XSS injection
   mailIdentities = [NSMutableArray arrayWithArray: [self arrayForKey: @"SOGoMailIdentities"]];
   for (i = 0 ; i < [mailIdentities length] ; i++) {
-    mailIdentity = [mailIdentities objectAtIndex: i];
-    if (mailIdentity && [mailIdentity objectForKey: @"fullName"] && [[self arrayForKey: @"SOGoMailIdentities"] isKindOfClass: [NSString class]]) {
-      fullName = [NSString stringWithString: [self arrayForKey: @"SOGoMailIdentities"]];
+    mailIdentity = [NSMutableDictionary dictionaryWithDictionary: [mailIdentities objectAtIndex: i]];
+    if (mailIdentity && [mailIdentity objectForKey: @"fullName"]) {
+      fullName = [NSString stringWithString: [mailIdentity objectForKey: @"fullName"]];
       if (fullName) {
         [mailIdentity setObject: [fullName stringWithoutHTMLInjection: YES] forKey: @"fullName"];
         [mailIdentities setObject: mailIdentity atIndexedSubscript: i];
