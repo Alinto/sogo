ef53f6c809fe4e7a62efaa11200d8de3bb10571a
smizrahi <seb.mizrahi@gmail.com>
2023-02-01 09:25:39 +0100

    fix(calendar): Fix inaccessibility to personal calendar on Mac OS X Ventura. Fixes #5639

--- SoObjects/Appointments/SOGoAppointmentFolder.m
+++ SoObjects/Appointments/SOGoAppointmentFolder.m
@@ -2548,7 +2548,7 @@ firstInstanceCalendarDateRange: (NGCalendarDateRange *) fir
                                 XMLNS_GROUPDAV, nil];
           [colType addObject: gdVTodoCol];
         }
-      if ([[nameInContainer lowercaseString] isEqualToString: @"personal"])
+      if ([nameInContainer isEqualToString: @"personal"])
         [colType addObject: [NSArray arrayWithObjects: @"schedule-outbox",
                                      XMLNS_CALDAV, nil]];
     }
