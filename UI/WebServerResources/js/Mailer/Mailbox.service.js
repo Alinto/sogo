0364f7f2a426041d010ad664172e6206c4bbfd6f
smizrahi <seb.mizrahi@gmail.com>
2024-12-09 17:40:07 +0100

    fix(mail): Fix issue where unread counter is invalid when whanging folder. Fixes #6064.

--- UI/WebServerResources/js/Mailer/Mailbox.service.js
+++ UI/WebServerResources/js/Mailer/Mailbox.service.js
@@ -1044,6 +1044,7 @@
     this.account = account;
     if (options && options.unseenCount) {
       this.unseenCount = options.unseenCount;
+      delete options["unseenCount"];
     }
     if (options && options.filter) {
       this.$messages = [];
